411b18fa1bcf2d8d2097f245ae1d4a038bd0c8cf
wallingTACC <walling@tacc.utexas.edu>
2019-01-16 11:52:17 -0600

    Adding script to get db stats.

---
+++ greenplum/database_stats.sql
@@ -0,0 +1,16 @@
+
+select
+   n.nspname,
+   relname,
+   --reloptions,
+   --relacl,
+   reltuples AS "#entries", pg_size_pretty(relpages::bigint*8*1024) AS size
+   FROM pg_class
+   JOIN pg_catalog.pg_namespace n ON n.oid = pg_class.relnamespace
+   WHERE relpages >= 8
+   and n.nspname in ('truven', 'optum')
+   ORDER BY n.nspname, relpages desc
+   
+   
+  select * 
+  from pg_namespace;
