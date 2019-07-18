8449f8e7e92d49bcbed70921c5de7c2537ddbda7
wcoughlin-uth <52790937+wcoughlin-uth@users.noreply.github.com>
2019-07-18 15:37:40 -0500

    Update optum-dod-mbrwdeath.sql

--- greenplum/optum/optum-dod-mbrwdeath.sql
+++ greenplum/optum/optum-dod-mbrwdeath.sql
@@ -1,14 +1,14 @@
 --Optum_DOD mbrwdeath load
 drop table optum_dod.member_wdeath;
 create table optum_dod.member_wdeath (
-PatID bigint, Death_year_month Date, Extract_year_month Date, VERSION numeric
+PatID bigint, Death_ym Date, Extract_ym Date, VERSION numeric
 ) 
 WITH (appendonly=true, orientation=column)
 distributed randomly;
 
 drop external table ext_member_wdeath;
 CREATE EXTERNAL TABLE ext_member_wdeath (
-PatID bigint, Death_year_month Date, Extract_year_month Date, VERSION numeric
+PatID bigint, Death_ym int, Extract_ym int, VERSION numeric
 ) 
 LOCATION ( 
 'gpfdist://c252-140:8801/dod_mbrwdeath.txt'
@@ -22,7 +22,11 @@ limit 1000;
 
 -- Insert
 insert into optum_dod.member_wdeath
-select * from ext_member_wdeath;
+select ex.PatID
+      ,(ex.Death_ym::varchar || '01')::date
+      ,(ex.Extract_ym::varchar || '01')::date
+      ,ex.Version
+from ext_member_wdeath ex;
 
 -- Analyze
 analyze optum_dod.member_wdeath;
