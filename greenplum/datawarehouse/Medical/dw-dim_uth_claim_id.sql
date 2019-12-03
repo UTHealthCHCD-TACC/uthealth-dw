0e39bb88f97fa71809be5cd9b34926a697dd56a9
wcoughlin-uth <william.c.coughlin@uth.tmc.edu>
2019-12-03 14:02:21 -0600

    claim detail

--- greenplum/datawarehouse/Medical/dw-dim_uth_claim_id.sql
+++ greenplum/datawarehouse/Medical/dw-dim_uth_claim_id.sql
@@ -7,7 +7,7 @@ create table data_warehouse.dim_uth_claim_id (
 	data_source char(4),
 	claim_id_src text not null,
 	member_id_src text not null,
-	data_year char(4) not null, 
+	data_year numeric(4)not null, 
 	uth_claim_id bigint,
 	uth_member_id bigint
 ) with (appendonly=true, orientation = column)
@@ -16,98 +16,81 @@ distributed by (generated_value);
 
 alter sequence data_warehouse.dim_uth_claim_id_generated_value_seq restart with 100000000;
 
+alter sequence data_warehouse.dim_uth_claim_id_generated_value_seq cache 200;
+
+
+alter table data_warehouse.dim_uth_claim_id alter column data_year type numeric(4,0) using(data_year::numeric(4,0));
 
 select dbo.set_all_perms();
 
-
-
-select * 
-from truven.ccaeo_wc;
-
-insert into data_warehouse.dim_uth_claim_id (data_source, claim_id_src, member_id_src, data_year , uth_member_id)
-
-
-select  'trvc', msclmid::text  , enrolid::text, trunc(year,0)::text, b.uth_member_id
---from truven.ccaes a
-from truven.ccaeo_wc a
-  join data_warehouse.dim_uth_member_id b 
-    on b.data_source = 'trvc'
-   and b.member_id_src = enrolid::text || '.0' 
-where a.enrolid is not null
- and not exists ( select 1 from data_warehouse.dim_uth_claim_id c
-				            where c.data_source = 'trvc'
-				              and c.claim_id_src = msclmid::text || '.0' 
-				              and c.member_id_src = a.enrolid::text || '.0' 
-				              and c.data_year = trunc(a.year,0)::text  )
-limit 10				              
-; ----------------------------------------------------------------------------------------------
-
- 
-
-explain analyze select distinct * into dev.ccaeo_2016_wc  from truven.ccaeo where year = 2016; 
-
-
-select count(*) from dev.ccaeo_2017_wc;
-
-select * from data_warehouse.dim_uth_claim_id;
- 
- 
- 
- 
- 
-insert into data_warehouse.dim_uth_claim_id (data_source, claim_id_src, member_id_src, data_year )
-
-
-select distinct 'trvc', msclmid::text, enrolid::text, trunc(year,0)::text, b.uth_member_id 
+insert into data_warehouse.dim_uth_claim_id (data_source, claim_id_src, member_id_src, data_year , uth_member_id)                                              
+select distinct  'trvc', a.msclmid::text, a.enrolid::text, trunc(a.year,0)::text, b.uth_member_id                                              
 from truven.ccaeo a
   join data_warehouse.dim_uth_member_id b 
     on b.data_source = 'trvc'
-   and b.member_id_src = enrolid::text 
-   
-   
-   
-  left outer join data_warehouse.dim_uth_claim_id c
-               on c.data_source = 'trvc'
-              and c.claim_id_src = msclmid::text
-              and c.member_id_src = enrolid::text
-              and c.data_year = trunc(year,0)::text
-where c.claim_id_src is null 
-and a.enrolid is not null
-and a.year = 2017;
+   and b.member_id_src = a.enrolid::text 
+  left join data_warehouse.dim_uth_claim_id c
+                                            on  b.data_source = c.data_source
+                                              and a.msclmid::text = c.claim_id_src 
+                                              and a.enrolid::text = c.member_id_src
+                                              and trunc(a.year,0)::text = c.data_year 
+  where a.enrolid is not null
+  and c.generated_value is null;
+ 
+ 
+insert into data_warehouse.dim_uth_claim_id (data_source, claim_id_src, member_id_src, data_year , uth_member_id)                                              
+select distinct  'trvc', coalesce(a.msclmid::text,a.caseid::text), a.enrolid::text, trunc(a.year,0)::text, b.uth_member_id                                              
+from truven.ccaes a  
+  join data_warehouse.dim_uth_member_id b 
+    on b.data_source = 'trvc'
+   and b.member_id_src = a.enrolid::text 
+  left join data_warehouse.dim_uth_claim_id c
+                                            on  b.data_source = c.data_source
+                                              and a.msclmid::text = c.claim_id_src 
+                                              and a.enrolid::text = c.member_id_src
+                                              and trunc(a.year,0)::text = c.data_year 
+  where a.enrolid is not null
+  and c.generated_value is null;
+ 
+--- Medicare outpatient 
+ insert into data_warehouse.dim_uth_claim_id (data_source, claim_id_src, member_id_src, data_year , uth_member_id)                                              
+select distinct  'trvc', a.msclmid::text, a.enrolid::text, trunc(a.year,0)::text, b.uth_member_id                                              
+from truven.mdcro a
+  join data_warehouse.dim_uth_member_id b 
+    on b.data_source = 'trvc'
+   and b.member_id_src = a.enrolid::text 
+  left join data_warehouse.dim_uth_claim_id c
+                                            on  b.data_source = c.data_source
+                                              and a.msclmid::text = c.claim_id_src 
+                                              and a.enrolid::text = c.member_id_src
+                                              and trunc(a.year,0)::text = c.data_year 
+  where a.enrolid is not null
+  and c.generated_value is null;
+ 
+ 
+ 
+--- Medicare inpatient  
+insert into data_warehouse.dim_uth_claim_id (data_source, claim_id_src, member_id_src, data_year , uth_member_id)     
+select distinct  'trvc', coalesce(a.msclmid::text,a.caseid::text), a.enrolid::text, trunc(a.year,0)::text, b.uth_member_id                                              
+from truven.mdcrs a  
+  join data_warehouse.dim_uth_member_id b 
+    on b.data_source = 'trvc'
+   and b.member_id_src = a.enrolid::text 
+  left join data_warehouse.dim_uth_claim_id c
+                                            on  b.data_source = c.data_source
+                                              and a.msclmid::text = c.claim_id_src 
+                                              and a.enrolid::text = c.member_id_src
+                                              and trunc(a.year,0)::text = c.data_year 
+  where a.enrolid is not null
+  and c.generated_value is null;
  
 
---trvc, 5554450.0, 33108316402.0, 2017
 
-select * from truven.ccaes where msclmid = 5554450.0 and year = 2017
-
-select a.msclmid,msclmid::text, a.enrolid, enrolid::text, a.year , b.* 
-from truven.ccaeo a
-  left outer join data_warehouse.dim_uth_claim_id b
-               on b.data_source = 'trvc'
-              and b.claim_id_src = msclmid::text
-              and b.member_id_src = enrolid::text
-              and b.data_year = trunc(year,0)::text
-where msclmid = 22768661.0 and enrolid = 4462286502.0 and year = 2017
-
-select * from data_warehouse.dim_uth_claim_id where claim_id_src = '1.0' and member_id_src = '720140604.0' and data_year = '2017'
-
-delete
-from data_warehouse.dim_uth_claim_id;
-
-
-update data_warehouse.dim_uth_claim_id set uth_claim_id = ( substring(data_year::text,3,2) || generated_value::text )::bigint;
-
-select count(*)
-from data_warehouse.dim_uth_claim_id --where claim_id_src = '308900219.0'
-
-
-select count(*), year from truven.ccaeo group by year;
-
-
-select enrolid, msclmid::text, year::text
-from truven.ccaeo_wc a 
-limit 1;
+update data_warehouse.dim_uth_claim_id set uth_claim_id = ( substring(data_year::text,3,2) || generated_value::text  )::bigint;
 
 
 
 
+select * , data_year::numeric(4,0) 
+from data_warehouse.dim_uth_claim_id
+where data_year = '';
