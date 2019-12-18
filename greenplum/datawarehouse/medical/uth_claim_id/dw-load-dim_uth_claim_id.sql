dcdc2469d95c1580cedaa9561bd702bb691d7fb1
wcoughlin-uth <william.c.coughlin@uth.tmc.edu>
2019-12-18 10:03:59 -0600

    uth_claim_id scripts

--- greenplum/datawarehouse/medical/uth_claim_id/dw-load-dim_uth_claim_id.sql
+++ greenplum/datawarehouse/medical/uth_claim_id/dw-load-dim_uth_claim_id.sql
@@ -4,13 +4,13 @@
  */
 
 ---truven commercial, outpatient
-insert into dw_qa.dim_uth_claim_id (data_source, claim_id_src, member_id_src, data_year , uth_member_id)                                              
+insert into data_warehouse.dim_uth_claim_id (data_source, claim_id_src, member_id_src, data_year , uth_member_id)                                              
 select distinct  'trvc', a.msclmid::text, a.enrolid::text, trunc(a.year,0), b.uth_member_id                                              
 from truven.ccaeo a
   join data_warehouse.dim_uth_member_id b 
     on b.data_source = 'trvc'
    and b.member_id_src = a.enrolid::text 
-  left join dw_qa.dim_uth_claim_id c
+  left join data_warehouse.dim_uth_claim_id c
          on  b.data_source = c.data_source
         and a.msclmid::text = c.claim_id_src 
         and a.enrolid::text = c.member_id_src
@@ -22,13 +22,13 @@ and c.uth_claim_id is null
 
  
 --truven commercial, inpatient 
-insert into dw_qa.dim_uth_claim_id (data_source, claim_id_src, member_id_src, data_year , uth_member_id)                                              
+insert into data_warehouse.dim_uth_claim_id (data_source, claim_id_src, member_id_src, data_year , uth_member_id)                                              
 select distinct  'trvc', coalesce(a.msclmid::text,a.caseid::text), a.enrolid::text, trunc(a.year,0), b.uth_member_id                                              
 from truven.ccaes a  
   join data_warehouse.dim_uth_member_id b 
     on b.data_source = 'trvc'
    and b.member_id_src = a.enrolid::text 
-  left join dw_qa.dim_uth_claim_id c
+  left join ddata_warehouse.dim_uth_claim_id c
 	    on  b.data_source = c.data_source
 	      and a.msclmid::text = c.claim_id_src 
 	      and a.enrolid::text = c.member_id_src
@@ -40,13 +40,13 @@ and c.uth_claim_id is null
  
 
 --- truven medicare outpatient 
-insert into dw_qa.dim_uth_claim_id (data_source, claim_id_src, member_id_src, data_year , uth_member_id)                                              
+insert into data_warehouse.dim_uth_claim_id (data_source, claim_id_src, member_id_src, data_year , uth_member_id)                                              
 select distinct  'trvm', a.msclmid::text, a.enrolid::text, trunc(a.year,0), b.uth_member_id                                              
 from truven.mdcro a
   join data_warehouse.dim_uth_member_id b 
     on b.data_source in ('trvm','trvc')
    and b.member_id_src = a.enrolid::text 
-  left join dw_qa.dim_uth_claim_id c
+  left join data_warehouse.dim_uth_claim_id c
     on  b.data_source = c.data_source
       and a.msclmid::text = c.claim_id_src 
       and a.enrolid::text = c.member_id_src
@@ -58,13 +58,13 @@ and c.uth_claim_id is null
  
  
 ---Truven medicare inpatient  
-insert into dw_qa.dim_uth_claim_id (data_source, claim_id_src, member_id_src, data_year , uth_member_id)     
+insert into data_warehouse.dim_uth_claim_id (data_source, claim_id_src, member_id_src, data_year , uth_member_id)     
 select distinct  'trvc', coalesce(a.msclmid::text,a.caseid::text), a.enrolid::text, trunc(a.year,0), b.uth_member_id                                              
 from truven.mdcrs a  
   join data_warehouse.dim_uth_member_id b 
     on b.data_source in ('trvm','trvc')
    and b.member_id_src = a.enrolid::text 
-  left join dw_qa.dim_uth_claim_id c
+  left join data_warehouse.dim_uth_claim_id c
                                             on  b.data_source = c.data_source
                                               and a.msclmid::text = c.claim_id_src 
                                               and a.enrolid::text = c.member_id_src
@@ -75,13 +75,13 @@ and c.uth_claim_id is null
 
 
 --Optum Dod 15min
-insert into dw_qa.dim_uth_claim_id (data_source, claim_id_src, member_id_src, data_year, uth_member_id)                                              
+insert into data_warehouse.dim_uth_claim_id (data_source, claim_id_src, member_id_src, data_year, uth_member_id)                                              
 select distinct  'optd', a.clmid::text, a.patid::text, trunc(a.year,0), b.uth_member_id                                              
 from optum_dod.medical a
   join data_warehouse.dim_uth_member_id b 
     on b.data_source = 'optd'
    and b.member_id_src = a.patid::text 
-  left join dw_qa.dim_uth_claim_id c
+  left join data_warehouse.dim_uth_claim_id c
         on  b.data_source = c.data_source
           and a.clmid::text = c.claim_id_src 
           and a.patid::text = c.member_id_src
@@ -91,13 +91,13 @@ and c.uth_claim_id is null
 ;
 
 --Optum Zip 15min
-insert into dw_qa.dim_uth_claim_id (data_source, claim_id_src, member_id_src, data_year, uth_member_id)                                              
+insert into data_warehouse.dim_uth_claim_id (data_source, claim_id_src, member_id_src, data_year, uth_member_id)                                              
 select distinct  'optz', a.clmid::text, a.patid::text, trunc(a.year,0), b.uth_member_id                                              
 from optum_zip.medical a
   join data_warehouse.dim_uth_member_id b 
     on b.data_source = 'optz'
    and b.member_id_src = a.patid::text 
-  left join dw_qa.dim_uth_claim_id c
+  left join data_warehouse.dim_uth_claim_id c
         on  b.data_source = c.data_source
           and a.clmid::text = c.claim_id_src 
           and a.patid::text = c.member_id_src
