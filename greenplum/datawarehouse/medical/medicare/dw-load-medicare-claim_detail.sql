8e26f82f5b6213135eef2e4c4dc27d04471af740
wcoughlin-uth <william.c.coughlin@uth.tmc.edu>
2020-02-27 15:22:06 -0600

    mdcr

--- greenplum/datawarehouse/medical/medicare/dw-load-medicare-claim_detail.sql
+++ greenplum/datawarehouse/medical/medicare/dw-load-medicare-claim_detail.sql
@@ -69,7 +69,7 @@ insert into dw_qa.claim_detail (  data_source, uth_claim_id, claim_sequence_numb
 								   claim_id_src, member_id_src, table_id_src )									  								   
 select 'mdcr', c.uth_claim_id, a.line_num::numeric, c.uth_member_id, b.clm_from_dt::date, b.clm_thru_dt::date,
 	       d.month_year_id, a.org_npi_num, null, null, a.line_place_of_srvc_cd, true, true, 
-	       null, null, a.hcpcs_cd,a.nch_clm_type_cd,  substring(a.hcpcs_1st_mdfr_cd,1,1),  substring(a.hcpcs_2nd_mdfr_cd,1,1), null,
+	       null, null, a.hcpcs_cd, a.nch_clm_type_cd,  substring(a.hcpcs_1st_mdfr_cd,1,1),  substring(a.hcpcs_2nd_mdfr_cd,1,1), null,
 	       a.carr_line_cl_chrg_amt::numeric, null, a.line_bene_pmt_amt::numeric , a.line_service_deductible::numeric, null, a.line_coinsrnc_amt::numeric, null, 
 	       null,null,null, a.line_srvc_cnt::numeric, null,
 	       b.clm_id, b.bene_id, 'bcarrier_claims_k'	 	      	      
@@ -96,9 +96,26 @@ insert into dw_qa.claim_detail (  data_source, uth_claim_id, claim_sequence_numb
 								   admit_date, discharge_date, procedure_cd, procedure_type, proc_mod_1, proc_mod_2, revenue_cd,
 								   charge_amount, allowed_amount, paid_amount, deductible, copay, coins, cob,
 								   bill_type_inst, bill_type_class, bill_type_freq, units, drg_cd,
-								   claim_id_src, member_id_src, table_id_src )		
-from medicare.dme_claims_k a
-
+								   claim_id_src, member_id_src, table_id_src )												   
+	select 'mdcr',c.uth_claim_id, a.line_num::numeric, c.uth_member_id, b.clm_from_dt::date, b.clm_thru_dt::date,
+	       d.month_year_id, a.prvdr_num, null, null, a.line_place_of_srvc_cd, true, true, 
+	       null, null, a.hcpcs_cd, a.nch_clm_type_cd,  substring(a.hcpcs_1st_mdfr_cd,1,1),  substring(a.hcpcs_2nd_mdfr_cd,1,1), null,
+	       a.line_sbmtd_chrg_amt::numeric, a.line_prmry_alowd_chrg_amt::numeric, a.line_bene_prmry_pyr_pd_amt::numeric, a.line_service_deductible::numeric, null, a.line_coinsrnc_amt::numeric, null,
+	       null, null, null, a.dmerc_line_mtus_cnt::numeric, null,
+	       a.clm_id, b.clm_id, 'dme_claims_k'
+from medicare.dme_line_k a
+  join medicare.dme_claims_k b
+     on a.bene_id = b.bene_id
+    and a.clm_id = b.clm_id 
+  join dw_qa.claim_header c 
+    on c.data_source = 'mdcr' 
+   and c.claim_id_src = a.clm_id
+   and c.member_id_src = a.bene_id
+ join reference_tables.ref_month_year d 
+    on d.month_int = extract(month from b.clm_from_dt::date)
+   and d.year_int = extract(year from b.clm_from_dt::date)
+where extract(year from b.clm_from_dt::date) between 2015 and 2017
+;
 
 
 --hha
@@ -108,8 +125,25 @@ insert into dw_qa.claim_detail (  data_source, uth_claim_id, claim_sequence_numb
 								   charge_amount, allowed_amount, paid_amount, deductible, copay, coins, cob,
 								   bill_type_inst, bill_type_class, bill_type_freq, units, drg_cd,
 								   claim_id_src, member_id_src, table_id_src )		
-from medicare.hha_base_claims_k a
-
+	select 'mdcr', c.uth_claim_id, a.clm_line_num::numeric, c.uth_member_id, b.clm_from_dt::date, b.clm_thru_dt::date,
+	       d.month_year_id, b.prvdr_num, null, null, b.clm_fac_type_cd, true, true, 
+	       null, null, a.hcpcs_cd,a.nch_clm_type_cd,  substring(a.hcpcs_1st_mdfr_cd,1,1),  substring(a.hcpcs_2nd_mdfr_cd,1,1), a.rev_cntr,
+	       a.rev_cntr_tot_chrg_amt::numeric, null, null, null, null, null, null, 
+	       substring(b.clm_fac_type_cd,1,1), substring(b.clm_srvc_clsfctn_type_cd,1,1), substring(b.clm_freq_cd,1,1), a.rev_cntr_unit_cnt::numeric, null,
+	       b.clm_id, b.bene_id, 'hha_revenue_center_k'
+from medicare.hha_revenue_center_k a 
+     join medicare.hha_base_claims_k b
+     on a.bene_id = b.bene_id
+    and a.clm_id = b.clm_id 
+  join dw_qa.claim_header c 
+    on c.data_source = 'mdcr' 
+   and c.claim_id_src = a.clm_id
+   and c.member_id_src = a.bene_id
+ join reference_tables.ref_month_year d 
+    on d.month_int = extract(month from b.clm_from_dt::date)
+   and d.year_int = extract(year from b.clm_from_dt::date)
+where extract(year from b.clm_from_dt::date) between 2015 and 2017
+;
 
 
 
@@ -120,8 +154,25 @@ insert into dw_qa.claim_detail (  data_source, uth_claim_id, claim_sequence_numb
 								   charge_amount, allowed_amount, paid_amount, deductible, copay, coins, cob,
 								   bill_type_inst, bill_type_class, bill_type_freq, units, drg_cd,
 								   claim_id_src, member_id_src, table_id_src )		
-from medicare.hospice_base_claims_k a
-
+	select 'mdcr', c.uth_claim_id, a.clm_line_num::numeric, c.uth_member_id, b.clm_from_dt::date, b.clm_thru_dt::date,
+	       d.month_year_id, b.prvdr_num, null, null, b.clm_fac_type_cd, true, true, 
+	       null, null, a.hcpcs_cd,a.nch_clm_type_cd,  substring(a.hcpcs_1st_mdfr_cd,1,1),  substring(a.hcpcs_2nd_mdfr_cd,1,1), a.rev_cntr,
+	       a.rev_cntr_tot_chrg_amt::numeric, null, null, null, null, null, null, 
+	       substring(b.clm_fac_type_cd,1,1), substring(b.clm_srvc_clsfctn_type_cd,1,1), substring(b.clm_freq_cd,1,1), a.rev_cntr_unit_cnt::numeric, null,
+	       b.clm_id, b.bene_id, 'hospice_base_claims_k'
+from medicare.hospice_revenue_center_k a 
+     join medicare.hospice_base_claims_k b
+     on a.bene_id = b.bene_id
+    and a.clm_id = b.clm_id 
+  join dw_qa.claim_header c 
+    on c.data_source = 'mdcr' 
+   and c.claim_id_src = a.clm_id
+   and c.member_id_src = a.bene_id
+ join reference_tables.ref_month_year d 
+    on d.month_int = extract(month from b.clm_from_dt::date)
+   and d.year_int = extract(year from b.clm_from_dt::date)
+where extract(year from b.clm_from_dt::date) between 2015 and 2017
+;
 
 --snf
 insert into dw_qa.claim_detail (  data_source, uth_claim_id, claim_sequence_number, uth_member_id, from_date_of_service, to_date_of_service,
@@ -130,8 +181,25 @@ insert into dw_qa.claim_detail (  data_source, uth_claim_id, claim_sequence_numb
 								   charge_amount, allowed_amount, paid_amount, deductible, copay, coins, cob,
 								   bill_type_inst, bill_type_class, bill_type_freq, units, drg_cd,
 								   claim_id_src, member_id_src, table_id_src )		
-from medicare.snf_base_claims_k a
-
+	select 'mdcr', c.uth_claim_id, a.clm_line_num::numeric, c.uth_member_id, b.clm_from_dt::date, b.clm_thru_dt::date,
+	       d.month_year_id, b.prvdr_num, null, null, b.clm_fac_type_cd, true, true, 
+	       null, null, a.hcpcs_cd,a.nch_clm_type_cd,  substring(a.hcpcs_1st_mdfr_cd,1,1),  substring(a.hcpcs_2nd_mdfr_cd,1,1), a.rev_cntr,
+	       a.rev_cntr_tot_chrg_amt::numeric, null, null, null, null, null, null, 
+	       substring(b.clm_fac_type_cd,1,1), substring(b.clm_srvc_clsfctn_type_cd,1,1), substring(b.clm_freq_cd,1,1), a.rev_cntr_unit_cnt::numeric, null,
+	       b.clm_id, b.bene_id, 'snf_base_claims_k'
+from medicare.snf_revenue_center_k a 
+     join medicare.snf_base_claims_k b
+     on a.bene_id = b.bene_id
+    and a.clm_id = b.clm_id 
+  join dw_qa.claim_header c 
+    on c.data_source = 'mdcr' 
+   and c.claim_id_src = a.clm_id
+   and c.member_id_src = a.bene_id
+ join reference_tables.ref_month_year d 
+    on d.month_int = extract(month from b.clm_from_dt::date)
+   and d.year_int = extract(year from b.clm_from_dt::date)
+where extract(year from b.clm_from_dt::date) between 2015 and 2017
+;
 
 
 
