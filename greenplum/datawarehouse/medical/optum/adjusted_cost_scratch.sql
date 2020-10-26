a5041dbfcedf0c5b2253d9ab755981042221c537
Tomislav Urban <tomislav.urban@gmail.com>
2020-10-26 13:20:17 -0500

    Added RX to cost adjustments.

--- greenplum/datawarehouse/medical/optum/adjusted_cost_scratch.sql
+++ greenplum/datawarehouse/medical/optum/adjusted_cost_scratch.sql
@@ -233,6 +233,115 @@ left outer join reference_tables.ref_optum_bill_type_from_tos bt on m.tos_cd=bt.
 where uth.data_source='optd';
 
 
+-- RX imports
+
+drop table if exists data_warehouse.pharmacy_claims;
+
+
+create table data_warehouse.pharmacy_claims ( 
+		data_source char(4),
+		year int2, 
+		-- NEW
+		year_adj int2,
+		-- END NEW
+		uth_rx_claim_id int8,
+		uth_member_id int8,
+		fill_date date,
+		ndc char(11) check (length(ndc)=11),
+		days_supply int2,
+		script_id text, 
+		refill_count int2,
+		month_year_id int4,
+		generic_ind char(1),
+		generic_name text,
+		brand_name text,
+		quantity int4, 
+		provider_npi text,
+		pharmacy_id text,
+		total_charge_amount numeric(13,2),
+		total_allowed_amount numeric(13,2),
+		total_paid_amount numeric(13,2),
+		-- NEW
+		total_charge_amount_adj numeric(13,2),
+		total_allowed_amount_adj numeric(13,2),
+		total_paid_amount_adj numeric(13,2),
+		-- END NEW
+		deductible numeric(13,2),
+		copay numeric(13,2),
+		coins numeric(13,2),
+		cob numeric(13,2),
+		rx_claim_id_src text,
+		member_id_src text
+)
+with (appendonly=true, orientation = column)
+distributed by (uth_member_id);
+
+
+---optum zip
+insert into data_warehouse.pharmacy_claims (
+		data_source, year, 
+		-- NEW
+		year_adj,
+		-- END NEW
+		uth_rx_claim_id, uth_member_id, script_id, 
+		ndc, days_supply, refill_count, fill_date, month_year_id, generic_ind, generic_name, brand_name, 
+		quantity, provider_npi, pharmacy_id, 
+		total_charge_amount, total_allowed_amount, total_paid_amount,
+		-- NEW
+		total_charge_amount_adj, total_allowed_amount_adj, total_paid_amount_adj,
+		-- END NEW
+		deductible, copay, coins, cob, rx_claim_id_src, member_id_src
+		)			
+select 'optz', extract(year from a.fill_dt), b.uth_rx_claim_id, b.uth_member_id, patid::text || lpad(ndc, 11,'0') || a.fill_dt,
+       lpad(ndc, 11,'0'), a.days_sup, a.rfl_nbr::numeric, a.fill_dt, c.month_year_id, a.gnrc_ind, a.gnrc_nm, a.brnd_nm,
+       a.quantity, a.prescriber_prov, a.pharm, a.charge, a.std_cost, null, 
+       a.deduct, a.copay, null, null, a.clmid, a.patid::text
+from optum_zip.rx a 
+  join data_warehouse.dim_uth_rx_claim_id b 
+     on b.data_source = 'optz' 
+    and b.member_id_src = a.patid::text
+    and b.rx_claim_id_src = a.clmid
+  join reference_tables.ref_month_year c 
+    on c.month_int = extract(month from a.fill_dt)
+    and c.year_int = extract(year from a.fill_dt)
+	-- NEW
+	join reference_tables.ref_optum_cost_factor cf on cf.service_type = 'PHARMA' and cf.standard_price_year = a.std_cost_yr::int
+	-- END NEW    
+ ;
+
+
+--optum dod
+insert into data_warehouse.pharmacy_claims (
+		data_source, year, 
+		-- NEW
+		year_adj,
+		-- END NEW
+		uth_rx_claim_id, uth_member_id, script_id, 
+		ndc, days_supply, refill_count, fill_date, month_year_id, generic_ind, generic_name, brand_name, 
+		quantity, provider_npi, pharmacy_id, 
+		total_charge_amount, total_allowed_amount, total_paid_amount,
+		-- NEW
+		total_charge_amount_adj, total_allowed_amount_adj, total_paid_amount_adj,
+		-- END NEW
+		deductible, copay, coins, cob, rx_claim_id_src, member_id_src
+		)			
+select 'optd', extract(year from a.fill_dt), b.uth_rx_claim_id, b.uth_member_id, patid::text || lpad(ndc, 11,'0') || a.fill_dt,
+       lpad(ndc, 11,'0'), a.days_sup, a.rfl_nbr::numeric, a.fill_dt, c.month_year_id, a.gnrc_ind, a.gnrc_nm, a.brnd_nm,
+       a.quantity, a.prescriber_prov, a.pharm, a.charge, a.std_cost, null, 
+       a.deduct, a.copay, null, null, a.clmid, a.patid::text 
+from optum_dod.rx a 
+  join data_warehouse.dim_uth_rx_claim_id b 
+     on b.data_source = 'optd' 
+    and b.member_id_src = a.patid::text
+    and b.rx_claim_id_src = a.clmid
+  join reference_tables.ref_month_year c 
+    on c.month_int = extract(month from a.fill_dt)
+    and c.year_int = extract(year from a.fill_dt)
+	-- NEW
+	join reference_tables.ref_optum_cost_factor cf on cf.service_type = 'PHARMA' and cf.standard_price_year = a.std_cost_yr::int
+	-- END NEW        
+ ;
+
 -- Insert
 --insert into optum_zip_refresh.confinement
 --select 0, * from ext_confinement;
