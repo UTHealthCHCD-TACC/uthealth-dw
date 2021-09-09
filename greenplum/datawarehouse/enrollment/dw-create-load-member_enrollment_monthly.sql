03deb4dc183d9ea0aba28fe3250b40fb9feb68be
Coughlin <wcoughlin@uth.tmc.edu>
2021-09-09 09:19:26 -0500

    permissions update wc 9/9/21

--- greenplum/datawarehouse/enrollment/dw-create-load-member_enrollment_monthly.sql
+++ greenplum/datawarehouse/enrollment/dw-create-load-member_enrollment_monthly.sql
@@ -46,7 +46,7 @@ create table dw_staging.member_enrollment_monthly  (
 	rx_coverage int2, 
 	fiscal_year int2,
 	row_id bigserial
-) distributed by (uth_member_id);
+) distributed by (member_id_src);
 
 ---
                                                                            
@@ -222,6 +222,15 @@ from truven.mdcrt m
 ---------------------------------------------------------------------------------------------------
 
 drop table dev.truven_uth_mem;
+
+
+vacuum analyze dw_staging.member_enrollment_monthly;
+
+select count(*), data_source, year 
+from dw_staging.member_enrollment_monthly mem 
+group by data_source, year  
+order by data_source, year 
+
 
 
 
@@ -448,63 +457,87 @@ from medicaid.chip_uth  a
 ;
 
 
--------(---------------- data loads --------------------)
+-------(^---------------- data loads --------------------^)
 
 
 
-------run below code for data sources vvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+------*****run below code for data sources***** vvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+
+---redistribute table on row id so below scripts run quicker
+create table dw_staging.temp_member_enrollment_monthly
+with (appendonly=true, orientation=column) as 
+select * from dw_staging.member_enrollment_monthly 
+distributed by (row_id);
+
+drop table if exists dw_staging.member_enrollment_monthly; 
+
+alter table dw_staging.temp_member_enrollment_monthly rename to dw_staging.member_enrollment_monthly;
+
 vacuum analyze dw_staging.member_enrollment_monthly;
 
----- delete duplicate rows
-delete from dw_staging.member_enrollment_monthly where row_identifier in ( 
-	select row_identifier
-	from
-	(		
-	select row_number() over(partition by uth_member_id, month_year_id order by month_year_id) as rn
-		      ,*
-		from dw_staging.member_enrollment_monthly 		
-	) sub
-	where rn > 1
-);
+
+
+---- get row id of duplicate rows
+drop table if exists  dev.temp_dupe_enrollment_rows;
+
+create table dev.temp_dupe_enrollment_rows
+with (appendonly=true, orientation=column) as 
+select row_id::bigint as row_id 
+from
+(		
+select row_number() over(partition by uth_member_id, month_year_id order by month_year_id) as rn
+	      ,*
+	from dw_staging.member_enrollment_monthly 		
+) sub
+where rn > 1
+distributed by (row_id);	
+
+--remote dupe records so only one per member per month - runtime: 12min
+delete from dw_staging.member_enrollment_monthly a 
+  using dev.temp_dupe_enrollment_rows b 
+   where a.row_id = b.row_id 
+; 
+ 	
+		
+---**script to build consecutive enrolled months	  16min	
+drop table if exists dev.temp_consec_enrollment;
+
+create table dev.temp_consec_enrollment 
+with (appendonly=true, orientation=column) as 
+select row_id::bigint as row_id
+      ,row_number() over(partition by uth_member_id, my_grp order by  month_year_id) as in_streak
+from ( 
+	   select a.row_id
+	         ,a.month_year_id
+	         ,a.uth_member_id
+	         ,b.my_row_counter - row_number() over(partition by a.uth_member_id order by a.month_year_id) as my_grp
+	   from dw_staging.member_enrollment_monthly 	 a 
+	     join reference_tables.ref_month_year b 
+	       on a.month_year_id = b.month_year_id 	   		    
+	 ) inr    
+distributed by (row_id);
 
 
 
-select * 
-from dw_staging.member_enrollment_monthly 
-where uth_member_id = 673299755
-
-
-	select row_number() over(partition by uth_member_id, month_year_id order by month_year_id) as rn
-		      ,*
-		from dw_staging.member_enrollment_monthly 	
-		where uth_member_id = 673299755
-
-
----logic to add consecutive enrolled months
----------------------------------------------
-with row_build_cte as ( 
-	select row_identifier 
-	      ,row_number() over(partition by uth_member_id, my_grp order by  month_year_id) as in_streak
-	from ( 
-		   select a.row_identifier
-		         ,a.month_year_id
-		         ,a.uth_member_id
-		         ,b.my_row_counter - row_number() over(partition by a.uth_member_id order by a.month_year_id) as my_grp
-		   from dw_staging.member_enrollment_monthly 	 a 
-		     join reference_tables.ref_month_year b 
-		       on a.month_year_id = b.month_year_id 	   		    
-		 ) sub    
-) 
-update dw_staging.member_enrollment_monthly c 
-set consecutive_enrolled_months = d.in_streak 
-from row_build_cte d
-where c.row_identifier = d.row_identifier
+--update consec enrolled months  9m 
+update dw_staging.member_enrollment_monthly a set consecutive_enrolled_months = b.in_streak 
+from dev.temp_consec_enrollment b 
+where a.row_id = b.row_id
 ;
 
+-----*****run above code for all data sets*****              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+--final
 vacuum analyze dw_staging.member_enrollment_monthly;
 
------run above code for all data sets              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-
 ----/END SCRIPT
+
+
+select data_source, year, count(*)
+from dw_staging.member_enrollment_monthly
+group by data_source, year
+order by data_source, year
+;
+
+
 
--- greenplum/permissions.sql
+++ greenplum/permissions.sql
@@ -1,108 +1,26 @@
-revoke all on schema public from public;
 
-/*
- * SSL Status
- */
-SELECT datname,usename, ssl, client_addr 
-  FROM pg_catalog.pg_stat_ssl
-  JOIN pg_catalog.pg_stat_activity
-    ON pg_stat_ssl.pid = pg_stat_activity.pid;
-
-   select * from pg_catalog.pg_stat_ssl
----making a note 
-/*
- * UTHealth Admin Role
- */
-create role uthealthadmin;
-grant connect on database uthealth to group uthealthdev;
-
-grant all on database uthealth to uthealthadmin;
-
---Schemas
-grant all on schema truven to group uthealthadmin;
-ALTER DEFAULT PRIVILEGES IN SCHEMA truven grant all on tables to group uthealthadmin; 
-GRANT all ON ALL TABLES IN SCHEMA truven TO uthealthadmin;
-
-grant all on schema optum_dod to group uthealthadmin;
-ALTER DEFAULT PRIVILEGES IN SCHEMA optum_dod grant all on tables to group uthealthadmin; 
-GRANT all ON ALL TABLES IN SCHEMA optum_dod TO uthealthadmin;
-
-grant all on schema optum_zip to group uthealthadmin;
-ALTER DEFAULT PRIVILEGES IN SCHEMA optum_zip grant all on tables to group uthealthadmin; 
-GRANT all ON ALL TABLES IN SCHEMA optum_zip TO uthealthadmin;
-
-grant all on schema reference_tables to group uthealthadmin;
-ALTER DEFAULT PRIVILEGES IN SCHEMA reference_tables grant all on tables to group uthealthadmin; 
-GRANT all ON ALL TABLES IN SCHEMA reference_tables TO uthealthadmin;
-
-grant all on schema conditions to group uthealthadmin;
-ALTER DEFAULT PRIVILEGES IN SCHEMA conditions grant all on tables to group uthealthadmin; 
-GRANT all ON ALL TABLES IN SCHEMA conditions TO uthealthadmin;
-
-grant all on schema data_warehouse to group uthealthadmin;
-ALTER DEFAULT PRIVILEGES IN SCHEMA data_warehouse grant all on tables to group uthealthadmin; 
-GRANT all ON ALL TABLES IN SCHEMA data_warehouse TO uthealthadmin;
-
-grant all on schema medicare_national to group uthealthadmin;
-ALTER DEFAULT PRIVILEGES IN SCHEMA medicare_national grant all on tables to group uthealthadmin; 
-GRANT all ON ALL TABLES IN SCHEMA medicare_national TO uthealthadmin;
-
-grant all on schema medicare_texas to group uthealthadmin;
-ALTER DEFAULT PRIVILEGES IN SCHEMA medicare_texas grant all on tables to group uthealthadmin; 
-GRANT all ON ALL TABLES IN SCHEMA medicare_texas TO uthealthadmin;
-
-grant all on schema medicaid to group uthealthadmin;
-ALTER DEFAULT PRIVILEGES IN SCHEMA medicaid grant all on tables to group uthealthadmin; 
-GRANT all ON ALL TABLES IN SCHEMA medicaid TO uthealthadmin;
-
-grant all on schema dev to group uthealthadmin;
-ALTER DEFAULT PRIVILEGES IN SCHEMA dev grant all on tables to group uthealthadmin; 
-GRANT all ON ALL TABLES IN SCHEMA dev TO uthealthadmin;
-
-grant all on schema dw_staging to group uthealthadmin;
-ALTER DEFAULT PRIVILEGES IN SCHEMA dw_staging grant all on tables to group uthealthadmin; 
-GRANT all ON ALL TABLES IN SCHEMA dw_staging TO uthealthadmin;
+/* ******************************************************************************************************
+ *  This table is used to generate a de-identified claim id that will be used to populate claim_detail and claim_header tables
+ *	The uth_claim_id column will be a sequence that is initially set to a 100,000,000
+ *  This code can be re-run as new data comes in, logic is in place to prevent duplicate entries into table
+ * ******************************************************************************************************
+ *  Author || Date      || Notes
+ * ******************************************************************************************************
+ *  wallingTACC  || 1/1/2019 || script created
+ * ******************************************************************************************************
+ *  wcc001  || 9/09/2021 || organized uthealth_analyst and uthealthdev roles. cleaned up unused code. added comment block
+ * -------------------------   script is divided into: Role Defintions, User Creation, & Audits/Misc
+ * ******************************************************************************************************
+ * 
+ * ****************************************************************************************************** */
 
 
-
-grant all on schema qa_reporting to group uthealthadmin;
-ALTER DEFAULT PRIVILEGES IN SCHEMA qa_reporting grant all on tables to group uthealthadmin; 
-GRANT all ON ALL TABLES IN SCHEMA qa_reporting TO uthealthadmin;
-
-
-grant usage on schema pg_aoseg to group uthealthadmin;
-GRANT select ON ALL TABLES IN SCHEMA pg_aoseg TO uthealthadmin;
-
-grant usage on schema pg_catalog to group uthealthadmin;
-GRANT select ON ALL TABLES IN SCHEMA pg_catalog TO uthealthadmin;
-
-grant usage on schema pg_bitmapindex to group uthealthadmin;
-GRANT select ON ALL TABLES IN SCHEMA pg_bitmapindex TO uthealthadmin;
-
-grant usage on schema gp_toolkit to group uthealthadmin;
-GRANT select ON ALL TABLES IN SCHEMA gp_toolkit TO uthealthadmin;
+---(----------  *****************  ----------------------------------------------------------------------------------------------------)  
+---(---------- Role Definitions    ----------------------------------------------------------------------------------------------------)
 
 /*
  * UTHealth uthealth_analyst Role
  */
-
---CREATE ROLE smadhuri NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN PASSWORD 'password';
-
-
-CREATE ROLE smadhuri NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN;
-
-grant uthealthadmin to turban;
-
-grant uthealth_analyst to smadhuri;
-
-grant uthealth_analyst to ctruong;
-
-grant uthealth_analyst to yliu26;
-
-grant uthealth_analyst to lghosh1;
-
-grant uthealth_analyst to jfu2;
-
 
 -- uthealth_analyst role definition
 drop owned by uthealth_analyst cascade;
@@ -113,83 +31,197 @@ create role uthealth_analyst;
 
 grant connect on database uthealth to uthealth_analyst;
 
---Schemas
+---****uthealth_analyst assigned users *****************************************************
+grant uthealth_analyst to cms2;
 
----conditions
+grant uthealth_analyst to lghosh1;
+
+grant uthealth_analyst to ctruong;
+
+grant uthealth_analyst to jharri66;
+
+grant uthealth_analyst to jwozny;
+
+grant uthealth_analyst to jfu2;
+
+grant uthealth_analyst to smadhuri;
+
+--***********************************************************************************************************
+
+--Schemas permissions for uthealth_analyst 
+
+---conditions (select only)
 grant usage on schema conditions to group uthealth_analyst; 
 grant select on all tables in schema conditions to group uthealth_analyst; 
-
---reference_tables (select only)
-grant usage on schema reference_tables to group uthealth_analyst;
-grant select on all tables in schema reference_tables to group uthealth_analyst;
-ALTER DEFAULT PRIVILEGES IN SCHEMA reference_tables grant select on tables to group uthealth_analyst; 
+grant select on all sequences in schema conditions to uthealth_analyst;
+alter default privileges in schema conditions grant select on tables to group uthealth_analyst;
 
 --data_warehouse (select only) 
-grant usage on schema data_warehouse to group uthealth_analyst;
-grant select on all tables in schema data_warehouse to group uthealth_analyst;
-ALTER DEFAULT PRIVILEGES IN SCHEMA data_warehouse grant select on tables to group uthealth_analyst; 
-
-
---data_warehouse (select only) 
-grant usage on schema medicare to group uthealth_analyst;
-grant select on all tables in schema medicare to group uthealth_analyst;
-ALTER DEFAULT PRIVILEGES IN SCHEMA medicare grant select on tables to group uthealth_analyst; 
-
---dw_staging (select only) 
-grant usage on schema dw_staging to group uthealth_analyst;
-grant select on all tables in schema dw_staging to group uthealth_analyst;
-ALTER DEFAULT PRIVILEGES IN SCHEMA dw_staging grant select on tables to group uthealth_analyst; 
-
---qa_reporting(select only)
-grant all on schema qa_reporting to group uthealth_analyst;
-grant all on all tables in schema qa_reporting to group uthealth_analyst;
-ALTER DEFAULT PRIVILEGES IN SCHEMA qa_reporting grant all on tables to group uthealth_analyst; 
-
-ALTER DEFAULT PRIVILEGES IN SCHEMA optum_dod grant all on tables to group uthealthadmin; 
-
+grant usage on schema data_warehouse to group uthealth_analyst; 
+grant select on all tables in schema data_warehouse to group uthealth_analyst; 
+grant select on all sequences in schema data_warehouse to uthealth_analyst;
+alter default privileges in schema data_warehouse grant select on tables to group uthealth_analyst;
 
 --dev (all access)
+grant all on schema dev to uthealth_analyst; 
+grant all on all tables in schema dev to uthealth_analyst; 
+grant all privileges on all sequences in schema dev to uthealth_analyst; 
+alter default privileges in schema dev grant all on tables to uthealth_analyst; 
 
-grant all on schema dev to analyst;
-grant all on all tables in schema dev to analyst;
-grant all privileges on all sequences in schema dev to analyst;
-alter default privileges in schema dev grant all privileges to group analyst;
+--dw_staging (select only )
+grant usage on schema dw_staging to group uthealth_analyst; 
+grant select on all tables in schema dw_staging to group uthealth_analyst; 
+grant select on all sequences in schema dw_staging to uthealth_analyst;
+alter default privileges in schema dw_staging grant select on tables to group uthealth_analyst;
 
---dw_qa (all access)
-grant all on schema dw_qa to analyst;
-grant all on all tables in schema dw_qa to analyst;
-grant all privileges on all sequences in schema dw_qa to analyst;
-alter default privileges in schema dw_qa grant all privileges to  analyst;
+--***raw tables (select only)
+	---medicaid
+	grant usage on schema medicaid to group uthealth_analyst; 
+	grant select on all tables in schema medicaid to group uthealth_analyst; 
+	alter default privileges in schema medicaid grant select on tables to group uthealth_analyst;
+	---medicare_national
+	grant usage on schema medicare_national to group uthealth_analyst; 
+	grant select on all tables in schema medicare_national to group uthealth_analyst; 
+	alter default privileges in schema medicare_national grant select on tables to group uthealth_analyst;
+	---medicare_texas
+	grant usage on schema medicare_texas to group uthealth_analyst; 
+	grant select on all tables in schema medicare_texas to group uthealth_analyst; 
+	alter default privileges in schema medicare_texas grant select on tables to group uthealth_analyst;
+	---optum_dod
+	grant usage on schema optum_dod to group uthealth_analyst; 
+	grant select on all tables in schema optum_dod to group uthealth_analyst; 
+	alter default privileges in schema optum_dod grant select on tables to group uthealth_analyst;
+	---optum_zip
+	grant usage on schema optum_zip to group uthealth_analyst; 
+	grant select on all tables in schema optum_zip to group uthealth_analyst; 
+	alter default privileges in schema optum_zip grant select on tables to group uthealth_analyst;
+	---truven
+	grant usage on schema truven to group uthealth_analyst; 
+	grant select on all tables in schema truven to group uthealth_analyst; 
+	alter default privileges in schema truven grant select on tables to group uthealth_analyst;
+
+--qa_reporting (select only )
+grant usage on schema qa_reporting to group uthealth_analyst; 
+grant select on all tables in schema qa_reporting to group uthealth_analyst; 
+grant select on all sequences in schema qa_reporting to uthealth_analyst;
+alter default privileges in schema qa_reporting grant select on tables to group uthealth_analyst;
+
+--reference_tables (select only )
+grant usage on schema reference_tables to group uthealth_analyst; 
+grant select on all tables in schema reference_tables to group uthealth_analyst; 
+grant select on all sequences in schema reference_tables to uthealth_analyst;
+alter default privileges in schema reference_tables grant select on tables to group uthealth_analyst;
 
 
---conditions (all access)
-grant all on schema conditions to analyst;
-grant all on all tables in schema conditions to analyst;
-grant all privileges on all sequences in schema conditions to analyst;
-alter default privileges in schema conditions grant all on tables to analyst;
 
---raw data tables (select only)
-grant usage on schema truven, medicare_national, medicare_texas, medicaid, optum_dod, optum_zip to group uthealth_analyst;
-grant select on all tables in schema truven, medicare_national, medicare_texas, medicaid, optum_dod, optum_zip to group uthealth_analyst;
-ALTER DEFAULT PRIVILEGES IN SCHEMA truven grant select on tables to group uthealth_analyst; 
-ALTER DEFAULT PRIVILEGES IN SCHEMA medicare_national grant select on tables to group uthealth_analyst;
-ALTER DEFAULT PRIVILEGES IN SCHEMA medicare_texas grant select on tables to group uthealth_analyst;
-ALTER DEFAULT PRIVILEGES IN SCHEMA medicaid grant select on tables to group uthealth_analyst;
-ALTER DEFAULT PRIVILEGES IN SCHEMA optum_zip grant select on tables to group uthealth_analyst;
-ALTER DEFAULT PRIVILEGES IN SCHEMA optum_dod grant select on tables to group uthealth_analyst;
+/*
+ * uthealth_dev Role
+ */
 
---conditions (select only)
-grant all on schema conditions to group uthealth_analyst;
-grant select on all tables in schema conditions to group uthealth_analyst;
-ALTER DEFAULT PRIVILEGES IN SCHEMA conditions grant select on tables to uthealth_analyst; 
+-- uthealth_dev role definition
+drop owned by uthealth_dev cascade;
+
+drop role uthealth_dev;
+
+create role uthealth_dev;
+
+grant connect on database uthealth to uthealth_dev;
+
+---****uthealth_analyst assigned users *****************************************************
+grant uthealth_analyst to uthealth_dev;
+
+grant uthealth_dev to jwozny;
+
+--***********************************************************************************************************
+
+--***Schemas permissions for uthealth_dev***
+ 
+---dw_staging (all access)
+grant all on schema dw_staging to uthealth_dev; 
+grant all on all tables in schema dw_staging to uthealth_dev; 
+grant all privileges on all sequences in schema dw_staging to uthealth_dev; 
+alter default privileges in schema dw_staging grant all on tables to uthealth_dev; 
+
+---qa_reporting (all access)
+grant all on schema qa_reporting to uthealth_dev; 
+grant all on all tables in schema qa_reporting to uthealth_dev; 
+grant all privileges on all sequences in schema qa_reporting to uthealth_dev; 
+alter default privileges in schema qa_reporting grant all on tables to uthealth_dev; 
+
+---reference_tables (all access)
+grant all on schema reference_tables to uthealth_dev; 
+grant all on all tables in schema reference_tables to uthealth_dev; 
+grant all privileges on all sequences in schema reference_tables to uthealth_dev; 
+alter default privileges in schema reference_tables grant all on tables to uthealth_dev; 
 
 
-grant all on schema qa_reporting to uthealth_analyst;
-grant all on all tables in schema qa_reporting to uthealth_analyst;
-grant all privileges on all sequences in schema qa_reporting to uthealth_analyst;
-alter default privileges in schema qa_reporting grant all privileges uthealth_analyst;
+
+/*
+ * UTHealthAdmin Role
+ */
 
 
+-- uthealthadmin role definition
+drop owned by uthealth_admin cascade;
+
+drop role uthealth_admin;
+
+create role uthealth_admin;
+
+grant connect on database uthealth to group uthealth_admin;
+
+---****uthealth_analyst assigned users *****************************************************
+grant uthealth_analyst to uthealth_admin;
+
+grant uthealth_dev to uthealth_admin;
+
+--******************************************************************************************
+
+
+--Schema permissions for uthealthadmin
+
+---data_warehouse (all access)
+grant all on schema data_warehouse to uthealth_admin; 
+grant all on all tables in schema data_warehouse to uthealth_admin; 
+grant all privileges on all sequences in schema data_warehouse to uthealth_admin; 
+alter default privileges in schema data_warehouse grant all on tables to uthealth_admin; 
+
+--***raw tables (all access)
+	---medicaid
+	grant all on schema medicaid to group uthealth_admin; 
+	grant all on all tables in schema medicaid to group uthealth_admin; 
+	alter default privileges in schema medicaid grant all on tables to uthealth_admin;
+	---medicare_national
+	grant all on schema medicare_national to group uthealth_admin; 
+	grant all on all tables in schema medicare_national to group uthealth_admin; 
+	alter default privileges in schema medicare_national grant all on tables to group uthealth_admin;
+	---medicare_texas
+	grant all on schema medicare_texas to group uthealth_admin; 
+	grant all on all tables in schema medicare_texas to group uthealth_admin; 
+	alter default privileges in schema medicare_texas grant all on tables to group uthealth_admin;
+	---optum_dod
+	grant all on schema optum_dod to group uthealth_admin; 
+	grant all on all tables in schema optum_dod to group uthealth_admin; 
+	alter default privileges in schema optum_dod grant all on tables to group uthealth_admin;
+	---optum_zip
+	grant all on schema optum_zip to group uthealth_admin; 
+	grant all on all tables in schema optum_zip to group uthealth_admin; 
+	alter default privileges in schema optum_zip grant all on tables to group uthealth_admin;
+	---truven
+	grant all on schema truven to group uthealth_admin; 
+	grant all on all tables in schema truven to group uthealth_admin; 
+	alter default privileges in schema truven grant all on tables to group uthealth_admin;
+
+
+---(----------  End Role Definitions    ----------------------------------------------------------------------------------------------------)
+---(----------  ********************   ----------------------------------------------------------------------------------------------------)
+
+
+
+
+
+---(----------  ********************   ----------------------------------------------------------------------------------------------------)
+---(----------  User Creation          ----------------------------------------------------------------------------------------------------)
 /*
  * Create User
  */
@@ -207,12 +239,6 @@ grant connect on database uthealth to dwtest;
 
 alter role uthtest nosuperuser;
 
-/*
- * Public Schema settings
- */
-REVOKE ALL ON SCHEMA pg_catalog FROM public;
-grant USAGE ON SCHEMA pg_catalog TO public;
-grant select on tables in SCHEMA pg_catalog TO public;
 
 /*
  * Password change
@@ -225,9 +251,29 @@ alter user uthtest with password 'uthtest';
 -- Grant superuser
 ALTER USER turban SUPERUSER; 
 
+
+---(----------  End User Creation          ----------------------------------------------------------------------------------------------------)
+---(----------  ********************   ----------------------------------------------------------------------------------------------------)
+
+
+
+
+
+---(----------  ******************** ----------------------------------------------------------------------------------------------------)
+---(----------  Audits/Misc          ----------------------------------------------------------------------------------------------------)
+
 /*
  * Audit Permissions
  */
+
+/*
+ * Public Schema settings
+ */
+REVOKE ALL ON SCHEMA pg_catalog FROM public;
+grant USAGE ON SCHEMA pg_catalog TO public;
+grant select on tables in SCHEMA pg_catalog TO public;
+
+
 select *
 FROM information_schema.role_table_grants
 where grantee='uthealth_analyst';
@@ -262,8 +308,6 @@ ORDER BY 1;
 
 
 
-
-
 select * from pg_catalog.pg_tables where tableowner = 'yliu26'
 
 
@@ -271,12 +315,8 @@ select * from pg_catalog.pg_tables where tableowner = 'yliu26'
 REVOKE CONNECT ON DATABASE coviddb FROM PUBLIC; 
 REVOKE ALL PRIVILEGES ON DATABASE uthealth FROM public;
 
-grant uthealth_analyst to cms2;
 
 revoke connect on database uthealth from amoosa1;
-
-CREATE ROLE test_user2 NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN PASSWORD 'd3f@ult$';
-
 
 select * from pg_user where usename='wcough';
 select * from pg_roles where rolname='wcough';
