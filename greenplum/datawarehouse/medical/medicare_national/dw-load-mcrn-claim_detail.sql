995001ec72a6bb9c31316a81177ad071e480ad7c
joe wozny <joseph.s.wozny@uth.tmc.edu>
2021-11-04 11:53:59 -0500

    t

--- greenplum/datawarehouse/medical/medicare_national/dw-load-mcrn-claim_detail.sql
+++ greenplum/datawarehouse/medical/medicare_national/dw-load-mcrn-claim_detail.sql
@@ -35,13 +35,16 @@ insert into dw_staging.claim_detail ( data_source, year, uth_member_id, uth_clai
                                       month_year_id, place_of_service, network_ind, network_paid_ind, admit_date, discharge_date, discharge_status, 
                                       cpt_hcpcs_cd, procedure_type, proc_mod_1, proc_mod_2, drg_cd, revenue_cd, charge_amount, allowed_amount, 
                                       paid_amount, copay, deductible, coins, cob, bill_type_inst, bill_type_class, bill_type_freq, 
-                                      units, fiscal_year, table_id_src, claim_sequence_number_src 
+                                      units, fiscal_year, table_id_src, claim_sequence_number_src,
+                                      bill_provider, ref_provider, other_provider, perf_rn_provider, perf_at_provider, perf_op_provider
                                      )								   							   
 select 'mcrn',  extract(year from a.clm_thru_dt::date), c.uth_member_id, c.uth_claim_id, a.clm_line_num::numeric, b.clm_from_dt::date, a.clm_thru_dt::date,
 	    d.month_year_id, b.clm_fac_type_cd, true, true, b.clm_admsn_dt::date, b.nch_bene_dschrg_dt::date, b.ptnt_dschrg_stus_cd, 
 	    a.hcpcs_cd, a.nch_clm_type_cd,  a.hcpcs_1st_mdfr_cd, a.hcpcs_2nd_mdfr_cd,  b.clm_drg_cd, a.rev_cntr, a.rev_cntr_ncvrd_chrg_amt::numeric, null, 
 	    null, null, null, null, null,  substring(b.clm_fac_type_cd,1,1),  substring(b.clm_srvc_clsfctn_type_cd,1,1),  substring(b.clm_freq_cd,1,1), 
-	    a.rev_cntr_unit_cnt::int, b.year::int2, 'inpatient_revenue_center', a.clm_line_num 
+	    a.rev_cntr_unit_cnt::int, b.year::int2, 'inpatient_revenue_center', a.clm_line_num,
+	    null as bill_provider, null as ref_provider, null as other_provider, 
+	    null as perf_rn_provider, null as perf_at_provider, null as perf_op_provider
 from medicare_national.inpatient_revenue_center_k a 
      join medicare_national.inpatient_base_claims_k b
        on b.bene_id = a.bene_id 
@@ -61,13 +64,16 @@ insert into dw_staging.claim_detail ( data_source, year, uth_member_id, uth_clai
                                       month_year_id, place_of_service, network_ind, network_paid_ind, admit_date, discharge_date, discharge_status, 
                                       cpt_hcpcs_cd, procedure_type, proc_mod_1, proc_mod_2, drg_cd, revenue_cd, charge_amount, allowed_amount, 
                                       paid_amount, copay, deductible, coins, cob, bill_type_inst, bill_type_class, bill_type_freq, 
-                                      units, fiscal_year, table_id_src, claim_sequence_number_src 
+                                      units, fiscal_year, table_id_src, claim_sequence_number_src,
+                                      bill_provider, ref_provider, other_provider, perf_rn_provider, perf_at_provider, perf_op_provider
                                      )								   							   
 select 'mcrn', extract(year from a.clm_thru_dt::date), c.uth_member_id, c.uth_claim_id, a.clm_line_num::numeric, a.rev_cntr_dt::date, a.clm_thru_dt::date,
 	    d.month_year_id, b.clm_fac_type_cd, true, true, b.clm_admsn_dt::date, b.nch_bene_dschrg_dt::date, b.ptnt_dschrg_stus_cd, 
 	    a.hcpcs_cd, a.nch_clm_type_cd,  a.hcpcs_1st_mdfr_cd, a.hcpcs_2nd_mdfr_cd,  null, a.rev_cntr, a.rev_cntr_ncvrd_chrg_amt::numeric, null, 
 	    null, null, null, null, null,  substring(b.clm_fac_type_cd,1,1),  substring(b.clm_srvc_clsfctn_type_cd,1,1),  substring(b.clm_freq_cd,1,1), 
-	    a.rev_cntr_unit_cnt::int, b.year::int2, 'hha_revenue_center', a.clm_line_num 
+	    a.rev_cntr_unit_cnt::int, b.year::int2, 'hha_revenue_center', a.clm_line_num,
+	    null as bill_provider, null as ref_provider, null as other_provider, 
+	    a.rndrng_physn_npi as perf_rn_provider, null as perf_at_provider, null as perf_op_provider
 from medicare_national.hha_revenue_center_k a 
      join medicare_national.hha_base_claims_k b
      on a.bene_id = b.bene_id
@@ -87,13 +93,16 @@ insert into dw_staging.claim_detail ( data_source, year, uth_member_id, uth_clai
                                       month_year_id, place_of_service, network_ind, network_paid_ind, admit_date, discharge_date, discharge_status, 
                                       cpt_hcpcs_cd, procedure_type, proc_mod_1, proc_mod_2, drg_cd, revenue_cd, charge_amount, allowed_amount, 
                                       paid_amount, copay, deductible, coins, cob, bill_type_inst, bill_type_class, bill_type_freq, 
-                                      units, fiscal_year, table_id_src, claim_sequence_number_src 
+                                      units, fiscal_year, table_id_src, claim_sequence_number_src,
+                                      bill_provider, ref_provider, other_provider, perf_rn_provider, perf_at_provider, perf_op_provider
                                      )								   							   
 select 'mcrn',  extract(year from a.clm_thru_dt::date), c.uth_member_id, c.uth_claim_id, a.clm_line_num::numeric, a.rev_cntr_dt::date, a.clm_thru_dt::date,
 	    d.month_year_id, b.clm_fac_type_cd, true, true, null, b.nch_bene_dschrg_dt::date, b.ptnt_dschrg_stus_cd, 
 	    a.hcpcs_cd, a.nch_clm_type_cd,  a.hcpcs_1st_mdfr_cd, a.hcpcs_2nd_mdfr_cd,  null, a.rev_cntr, a.rev_cntr_ncvrd_chrg_amt::numeric, null, 
 	    null, null, null, null, null,  substring(b.clm_fac_type_cd,1,1),  substring(b.clm_srvc_clsfctn_type_cd,1,1),  substring(b.clm_freq_cd,1,1), 
-	    a.rev_cntr_unit_cnt::int, b.year::int2, 'hospice_revenue_center', a.clm_line_num 
+	    a.rev_cntr_unit_cnt::int, b.year::int2, 'hospice_revenue_center', a.clm_line_num,
+	    null as bill_provider, null as ref_provider, null as other_provider, 
+	    a.rndrng_physn_npi as perf_rn_provider, null as perf_at_provider, null as perf_op_provider
 from medicare_national.hospice_revenue_center_k a 
      join medicare_national.hospice_base_claims_k b
      on a.bene_id = b.bene_id
@@ -112,13 +121,16 @@ insert into dw_staging.claim_detail ( data_source, year, uth_member_id, uth_clai
                                       month_year_id, place_of_service, network_ind, network_paid_ind, admit_date, discharge_date, discharge_status, 
                                       cpt_hcpcs_cd, procedure_type, proc_mod_1, proc_mod_2, drg_cd, revenue_cd, charge_amount, allowed_amount, 
                                       paid_amount, copay, deductible, coins, cob, bill_type_inst, bill_type_class, bill_type_freq, 
-                                      units, fiscal_year, table_id_src, claim_sequence_number_src 
+                                      units, fiscal_year, table_id_src, claim_sequence_number_src,
+                                      bill_provider, ref_provider, other_provider, perf_rn_provider, perf_at_provider, perf_op_provider
                                      )								   							   
 select 'mcrn',  extract(year from a.clm_thru_dt::date), c.uth_member_id, c.uth_claim_id, a.clm_line_num::numeric, b.clm_from_dt::date, a.clm_thru_dt::date,
 	    d.month_year_id, b.clm_fac_type_cd, true, true, b.clm_admsn_dt::date, b.nch_bene_dschrg_dt::date, b.ptnt_dschrg_stus_cd, 
 	    a.hcpcs_cd, a.nch_clm_type_cd,  a.hcpcs_1st_mdfr_cd, a.hcpcs_2nd_mdfr_cd,  null, a.rev_cntr, a.rev_cntr_ncvrd_chrg_amt::numeric, null, 
 	    null, null, null, null, null,  substring(b.clm_fac_type_cd,1,1),  substring(b.clm_srvc_clsfctn_type_cd,1,1),  substring(b.clm_freq_cd,1,1), 
-	    a.rev_cntr_unit_cnt::int, b.year::int2, 'snf_revenue_center', a.clm_line_num 
+	    a.rev_cntr_unit_cnt::int, b.year::int2, 'snf_revenue_center', a.clm_line_num,
+	    null as bill_provider, null as ref_provider, null as other_provider, 
+	    null as perf_rn_provider, null as perf_at_provider, null as perf_op_provider
 from medicare_national.snf_revenue_center_k a 
      join medicare_national.snf_base_claims_k b
      on a.bene_id = b.bene_id
@@ -138,13 +150,16 @@ insert into dw_staging.claim_detail ( data_source, year, uth_member_id, uth_clai
                                       month_year_id, place_of_service, network_ind, network_paid_ind, admit_date, discharge_date, discharge_status, 
                                       cpt_hcpcs_cd, procedure_type, proc_mod_1, proc_mod_2, drg_cd, revenue_cd, charge_amount, allowed_amount, 
                                       paid_amount, copay, deductible, coins, cob, bill_type_inst, bill_type_class, bill_type_freq, 
-                                      units, fiscal_year, table_id_src, claim_sequence_number_src 
+                                      units, fiscal_year, table_id_src, claim_sequence_number_src,
+                                      bill_provider, ref_provider, other_provider, perf_rn_provider, perf_at_provider, perf_op_provider
                                      )								   							   
 select 'mcrn',  extract(year from a.clm_thru_dt::date), c.uth_member_id, c.uth_claim_id, a.clm_line_num::numeric, a.rev_cntr_dt::date, a.clm_thru_dt::date,
 	    d.month_year_id, b.clm_fac_type_cd, true, true, null, null, null, 
 	    a.hcpcs_cd, a.nch_clm_type_cd,  a.hcpcs_1st_mdfr_cd, a.hcpcs_2nd_mdfr_cd,  null, a.rev_cntr, a.rev_cntr_ncvrd_chrg_amt::numeric, null, 
 	    null, null, null, null, null,  substring(b.clm_fac_type_cd,1,1),  substring(b.clm_srvc_clsfctn_type_cd,1,1),  substring(b.clm_freq_cd,1,1), 
-	    a.rev_cntr_unit_cnt::int, b.year::int2, 'outpatient_revenue_center', a.clm_line_num 
+	    a.rev_cntr_unit_cnt::int, b.year::int2, 'outpatient_revenue_center', a.clm_line_num,
+	    null as bill_provider, null as ref_provider, null as other_provider, 
+	    a.rndrng_physn_npi as perf_rn_provider, null as perf_at_provider, null as perf_op_provider
 from medicare_national.outpatient_revenue_center_k a 
      join medicare_national.outpatient_base_claims_k b
        on b.bene_id = a.bene_id 
@@ -165,13 +180,16 @@ insert into dw_staging.claim_detail ( data_source, year, uth_member_id, uth_clai
                                       month_year_id, place_of_service, network_ind, network_paid_ind, admit_date, discharge_date, discharge_status, 
                                       cpt_hcpcs_cd, procedure_type, proc_mod_1, proc_mod_2, drg_cd, revenue_cd, charge_amount, allowed_amount, 
                                       paid_amount, copay, deductible, coins, cob, bill_type_inst, bill_type_class, bill_type_freq, 
-                                      units, fiscal_year, table_id_src, claim_sequence_number_src 
+                                      units, fiscal_year, table_id_src, claim_sequence_number_src,
+                                      bill_provider, ref_provider, other_provider, perf_rn_provider, perf_at_provider, perf_op_provider
                                      )								   							   
 select 'mcrn',  extract(year from a.clm_thru_dt::date), c.uth_member_id, c.uth_claim_id, a.line_num::numeric, b.clm_from_dt::date, a.clm_thru_dt::date,
 	    d.month_year_id, a.line_place_of_srvc_cd, true, true, null, null, null, 
 	    a.hcpcs_cd, a.nch_clm_type_cd,  a.hcpcs_1st_mdfr_cd, a.hcpcs_2nd_mdfr_cd,  null, null as reveneue_cd, a.line_alowd_chrg_amt::numeric, null, 
 	    a.line_bene_pmt_amt::numeric, null, a.line_service_deductible::numeric, a.line_coinsrnc_amt::numeric, null, null, null, null, 
-	    a.line_srvc_cnt::numeric, b.year::int2, 'bcarrier_line', a.line_num    	      
+	    a.line_srvc_cnt::numeric, b.year::int2, 'bcarrier_line', a.line_num,
+	    a.org_npi_num as bill_provider, null as ref_provider, null as other_provider, 
+	    a.prf_physn_npi as perf_rn_provider, null as perf_at_provider, null as perf_op_provider
 from medicare_national.bcarrier_line_k a 
      join medicare_national.bcarrier_claims_k b
        on b.bene_id = a.bene_id 
@@ -191,13 +209,16 @@ insert into dw_staging.claim_detail ( data_source, year, uth_member_id, uth_clai
                                       month_year_id, place_of_service, network_ind, network_paid_ind, admit_date, discharge_date, discharge_status, 
                                       cpt_hcpcs_cd, procedure_type, proc_mod_1, proc_mod_2, drg_cd, revenue_cd, charge_amount, allowed_amount, 
                                       paid_amount, copay, deductible, coins, cob, bill_type_inst, bill_type_class, bill_type_freq, 
-                                      units, fiscal_year, table_id_src, claim_sequence_number_src 
+                                      units, fiscal_year, table_id_src, claim_sequence_number_src,
+                                      bill_provider, ref_provider, other_provider, perf_rn_provider, perf_at_provider, perf_op_provider
                                      )								   							   
 select 'mcrn',  extract(year from a.clm_thru_dt::date), c.uth_member_id, c.uth_claim_id, a.line_num::numeric, b.clm_from_dt::date, a.clm_thru_dt::date,
 	    d.month_year_id, a.line_place_of_srvc_cd, true, true, null, null, null, 
 	    a.hcpcs_cd, a.nch_clm_type_cd,  a.hcpcs_1st_mdfr_cd, a.hcpcs_2nd_mdfr_cd,  null, null as reveneue_cd, a.line_alowd_chrg_amt::numeric, null, 
 	    a.line_bene_pmt_amt::numeric, null, a.line_service_deductible::numeric, a.line_coinsrnc_amt::numeric, null, null, null, null, 
-	    a.line_srvc_cnt::numeric, b.year::int2, 'dme_line', a.line_num    	    
+	    a.line_srvc_cnt::numeric, b.year::int2, 'dme_line', a.line_num,
+	    a.prvdr_npi as bill_provider, null as ref_provider, null as other_provider, 
+	    null as perf_rn_provider, null as perf_at_provider, null as perf_op_provider
 from medicare_national.dme_line_k a
   join medicare_national.dme_claims_k b
      on a.clm_id = b.clm_id 
