93f49986bf5657cf50b6a185774347b6bb53dde3
joe wozny <joseph.s.wozny@uth.tmc.edu>
2021-12-01 10:27:55 -0600

    add procedure type to medicaid

--- greenplum/datawarehouse/medical/medicaid/dw-load-medicaid-claim_detail.sql
+++ greenplum/datawarehouse/medical/medicaid/dw-load-medicaid-claim_detail.sql
@@ -13,7 +13,7 @@
 =======
  *  gmunoz  || 10/25/2021 || adding dev.fiscal_year_func() logic
  * ************************************************************************************************************ 
- *  jw003  || 11/29/2021  || changed discharge status to pat_stat from enc_header for the encounter table
+ *  jw003  || 11/29/2021  || changed discharge status to pat_stat from enc_header for the encounter table + added logic for procedure_type
  * ************************************************************************************************************
  * 
  * */
@@ -84,7 +84,8 @@ vacuum analyze dw_staging.claim_detail;
 insert into dw_staging.claim_detail ( data_source, year, uth_claim_id, claim_sequence_number, uth_member_id, 
                                      from_date_of_service, to_date_of_service, month_year_id, place_of_service,
                                      network_ind, network_paid_ind, admit_date, discharge_date, cpt_hcpcs_cd, 
-                                     procedure_type, proc_mod_1, proc_mod_2, revenue_cd, 
+                                     procedure_type, 
+                                     proc_mod_1, proc_mod_2, revenue_cd, 
                                      charge_amount, allowed_amount, paid_amount, 
                                      copay, deductible, coins, cob, 
                                      bill_type_inst, bill_type_class, bill_type_freq, 
@@ -95,7 +96,9 @@ insert into dw_staging.claim_detail ( data_source, year, uth_claim_id, claim_seq
 select 'mdcd', extract(year from a.from_dos) as year, c.uth_claim_id, null, c.uth_member_id, 
        a.from_dos, a.to_dos, get_my_from_date(a.from_dos) as month_year, trim(a.pos), 
        true, true, case when d.adm_dt = '' then null else d.adm_dt::date end , case when d.dis_dt = '' then null else d.dis_dt::date end, a.proc_cd, 
-       null, proc_mod_1, proc_mod_2, 
+        case when substring(proc_cd,1,1) ~ '[0-9]' then 'CPT'
+	   	     when substring(proc_cd,1,1) ~ '[a-zA-Z]' then 'HCPCS' else null end as procedure_type, 
+	   proc_mod_1, proc_mod_2, 
        case when isdigit(rev_cd) is false then null 
             when length(rev_cd) > 4 then null 
             else lpad(rev_cd,4,'0') end as revenue_code,  
@@ -147,7 +150,9 @@ insert into dw_staging.claim_detail ( data_source, year, uth_claim_id, claim_seq
 select 'mdcd', extract(year from a.fdos_dt::date), c.uth_claim_id, null, c.uth_member_id, 
        a.fdos_dt::date, a.tdos_csl::date, get_my_from_date(a.fdos_dt::date) as month_year, trim(a.pos),
        true, true, d.adm_dt::date, d.dis_dt::date, a.proc_cd, 
-       null,  proc_mod_cd_1, proc_mod_cd_2,
+       case when substring(proc_cd,1,1) ~ '[0-9]' then 'CPT'
+	   	    when substring(proc_cd,1,1) ~ '[a-zA-Z]' then 'HCPCS' else null end as procedure_type, 
+       proc_mod_cd_1, proc_mod_cd_2,
        case when isdigit(rev_cd) is false then null 
             when length(rev_cd) > 4 then null 
             else lpad(rev_cd,4,'0') end as revenue_code,  
