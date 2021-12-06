b87c755fcef2d2c6d67272485592507360c111f3
datagarret-uth <91634307+datagarret-uth@users.noreply.github.com>
2021-12-06 17:26:06 -0600

    Update cms_code_loader.py
    added bulk csv load, and a icd9 and icd 10 table

--- greenplum/datawarehouse/reference tables/cms_code_loader.py
+++ greenplum/datawarehouse/reference tables/cms_code_loader.py
@@ -1,3 +1,4 @@
+import io
 import psycopg2
 import psycopg2.extras
 import pandas as pd
@@ -31,13 +32,16 @@ def create_cms_icd_code_tables(cursor):
 file_df = pd.read_csv('./cms_codes.csv')
 file_df['code_description'] = file_df['code_description'].str.replace('"','')
 
+f = io.StringIO()
+file_df.to_csv(f, index=False, header=False, sep='\t')
+f.seek(0)
+
 with con.cursor() as cursor:
     create_cms_code_tables(cursor)
-    tuples = [tuple(x) for x in file_df.to_numpy()]
-    cols = ','.join(list(file_df.columns))
-    query = "insert into %s(%s) values %%s" % ("reference_tables.ref_cms_codes", cols)
-    psycopg2.extras.execute_values(cursor, query, tuples, page_size=5000)
     
+    cursor.execute('set search_path to reference_tables, public')
+    cursor.copy_from(f, 'ref_cms_codes', sep='\t', columns=('cd_type','cd_value','initial_year', 'last_year','code_description'))
+
     create_cms_icd_code_tables(cursor)
     cursor.execute("""insert into reference_tables.ref_cms_icd_codes select * from 
                       reference_tables.ref_cms_codes where cd_type in ('ICD10-CM', 'ICD9-CM');""")
