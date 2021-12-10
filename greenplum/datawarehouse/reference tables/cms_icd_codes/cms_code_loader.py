a7f35552c1a857023ccdff4f7e515d563c304348
datagarret-uth <91634307+datagarret-uth@users.noreply.github.com>
2021-12-10 11:35:02 -0600

    Update package directory
    updated paths to search the uth_helpers package

--- greenplum/datawarehouse/reference tables/cms_icd_codes/cms_code_loader.py
+++ greenplum/datawarehouse/reference tables/cms_icd_codes/cms_code_loader.py
@@ -2,11 +2,10 @@ import io
 import psycopg2
 import psycopg2.extras
 import pandas as pd
+import sys
+sys.path.append('H:/uth_helpers/')
+from uth_helpers.db_utils import get_dsn
 
-con = psycopg2.connect(database='uthealth', host='greenplum01.cdorral.tacc.utexas.edu', 
-                       user='user', port=5432, password="password")
-
-con.autocommit = True
 
 def create_cms_code_tables(cursor):
     cursor.execute("""
@@ -21,6 +20,7 @@ def create_cms_code_tables(cursor):
     distributed randomly;
     """)
 
+
 def create_cms_icd_cm_code_table(cursor):
     cursor.execute("""
     drop table if exists reference_tables.ref_cms_icd_cm_codes;
@@ -28,6 +28,7 @@ def create_cms_icd_cm_code_table(cursor):
     insert into reference_tables.ref_cms_icd_cm_codes select * from 
                       reference_tables.ref_cms_codes where cd_type in ('ICD10-CM', 'ICD9-CM');
     """)
+
 
 def create_cms_icd_pcs_code_table(cursor):
     cursor.execute("""
@@ -38,18 +39,28 @@ def create_cms_icd_pcs_code_table(cursor):
     """)
 
 
-file_df = pd.read_csv('./cms_codes.csv')
-file_df['code_description'] = file_df['code_description'].str.replace('"','')
+if __name__ == '__main__':
 
-f = io.StringIO()
-file_df.to_csv(f, index=False, header=False, sep='\t')
-f.seek(0)
+    con = psycopg2.connect(get_dsn())
+    con.autocommit = True
 
-with con.cursor() as cursor:
-    create_cms_code_tables(cursor)
-    
-    cursor.execute('set search_path to reference_tables, public')
-    cursor.copy_from(f, 'ref_cms_codes', sep='\t', columns=('cd_type','cd_value','initial_year', 'last_year','code_description'))
+    file_df = pd.read_csv('./cms_codes.csv')
+    file_df['code_description'] = file_df['code_description'].str.replace('"','')
 
-    create_cms_icd_cm_code_table(cursor)
-    create_cms_icd_pcs_code_table(cursor)
+    try:
+        with con.cursor() as cursor:
+            
+            create_cms_code_tables(cursor)
+            f = io.StringIO()
+            file_df.to_csv(f, index=False, header=False, sep='\t')
+            f.seek(0)
+            cursor.execute('set search_path to reference_tables, public')
+            cursor.copy_from(f, 'ref_cms_codes', sep='\t', columns=('cd_type','cd_value','initial_year', 'last_year','code_description'))
+
+            create_cms_icd_cm_code_table(cursor)
+            create_cms_icd_pcs_code_table(cursor)
+    except:
+        raise
+
+    finally:
+        con.close()
--- greenplum/datawarehouse/reference tables/fda_ndc/fda_ndc_getter.py
+++ greenplum/datawarehouse/reference tables/fda_ndc/fda_ndc_getter.py
@@ -1,22 +1,11 @@
 # FDA has historical and current ndc through their API
-import requests 
-import zipfile 
-import io
-import os
-
-
-def get_fda_ndc(output_dir, url):
-    try:
-        os.mkdir(output_dir)
-        r = requests.get(url)
-        z = zipfile.ZipFile(io.BytesIO(r.content))
-        z.extractall(output_dir)
-        print('Zip Extracted')
-    except:
-        print('File already exists')
+import sys
+sys.path.append('H:/uth_helpers/')
+from uth_helpers import helpers
 
 if __name__ == '__main__':
 
-    output_dir = './fda_ndc_output'
+    output_dir = './fda_ndc_output_test'
     url = 'https://download.open.fda.gov/drug/ndc/drug-ndc-0001-of-0001.json.zip'
-    get_fda_ndc(output_dir, url)
+    
+    helpers.url_zip_outputter(output_dir, url)
