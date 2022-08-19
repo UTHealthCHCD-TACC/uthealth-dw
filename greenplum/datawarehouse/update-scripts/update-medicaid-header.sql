19b62e2f78f97ec6d5dbd76693279678aaf97f41
isrraelp <isrrael.perez@uth.tmc.edu>
2022-08-19 14:00:06 -0500

    updating update scripts

--- greenplum/datawarehouse/update-scripts/update-medicaid-header.sql
+++ greenplum/datawarehouse/update-scripts/update-medicaid-header.sql
@@ -15,3 +15,73 @@
 7) run it 
 */
 
+drop table if exists dev.ip_medicaid_header_update;
+
+-- 42s
+select * 
+into dev.ip_medicaid_header_update
+from data_warehouse.claim_header 
+where data_source = 'mdcd'
+--limit 5000000;
+
+-- medicaid claims 2m 42s
+with cte_pos as (select max(pos) as pos, icn
+				   from medicaid.clm_detail 
+				   group by icn)
+update dev.ip_medicaid_header_update c
+   set claim_type = case when pos.pos = '1' then 'P' else 'F' end
+  from medicaid.clm_header h
+  join medicaid.clm_proc p
+    on h.icn = p.icn
+  join data_warehouse.dim_uth_claim_id u
+    on u.claim_id_src = p.icn
+   and u.member_id_src = p.pcn
+   and u.data_source = 'mdcd'
+  left outer join cte_pos as pos
+    on h.icn = pos.icn
+ where c.uth_claim_id = u.uth_claim_id;
+ 
+-- medicaid encounters 2m 56s
+update dev.ip_medicaid_header_update c
+   set claim_type = case when h.tx_cd = 'P' then 'P'
+   						when h.tx_cd = 'I' then 'F'
+   						else 'n' end -- change to null when updating dw table
+  from medicaid.enc_header h
+  join medicaid.enc_proc p
+    on h.derv_enc = p.derv_enc
+  join data_warehouse.dim_uth_claim_id u
+    on u.claim_id_src = p.derv_enc
+   and u.member_id_src = p.mem_id
+   and u.data_source = 'mdcd'
+ where c.uth_claim_id = u.uth_claim_id;
+ 
+-- QA, claim type counts for updated table and raw data 
+-- 8s
+with cte as (
+select case when pos = '1' then 'P' else 'F' end as claim_type
+ from (select max(pos) as pos, icn
+	    from medicaid.clm_detail 
+ 	   group by icn) a
+	   union all
+	  select case when tx_cd = 'P' then 'P'
+   					when tx_cd = 'I' then 'F'
+   					else 'n' end
+		from medicaid.enc_header)
+select *
+from (
+select 'update', 'total' as claim_type, count(*)
+  from dev.ip_medicaid_header_update
+ group by 1,2
+union
+select 'update', claim_type, count(claim_type)
+  from dev.ip_medicaid_header_update
+ group by 1,2
+union
+select 'raw', 'total', count(*)
+  from cte
+group by 1,2
+union
+select 'raw', claim_type, count(claim_type)
+  from cte
+  group by 1,2) counts
+order by 1,2;	
--- greenplum/datawarehouse/update-scripts/update-medicare-cost.sql
+++ greenplum/datawarehouse/update-scripts/update-medicare-cost.sql
@@ -13,7 +13,52 @@
 6) whenever ur done let joe know look over it 
 7) run it 
 */
-update table set blah blah = 
-where id = id and clmid = clmid;
+--update table set blah blah = 
+--where id = id and clmid = clmid;
+
+drop table if exists dev.ip_medicare_rx_cost_update;
+
+select *
+  into dev.ip_medicare_rx_cost_update
+  from data_warehouse.pharmacy_claims
+ where data_source in ('mcrt', 'mcrn')
+-- limit 100000000;
+ 
+select 'total' as group, count(*) from dev.ip_medicare_rx_cost_update
+union
+select data_source, count(data_source) from dev.ip_medicare_rx_cost_update
+group by 1;
 
 
+-- updating allowed amounts and charged amounts
+-- medicare texas 5m 36s
+update dev.ip_medicare_rx_cost_update c
+   set total_allowed_amount = a.tot_rx_cst_amt::numeric,
+   		total_charge_amount = null
+  from medicare_texas.pde_file a
+  join data_warehouse.dim_uth_rx_claim_id b
+    on b.data_source in ('mcrt')
+   and a.bene_id = b.member_id_src 
+   and a.pde_id = b.rx_claim_id_src
+  where c.uth_rx_claim_id = b.uth_rx_claim_id;
+ 
+-- medicare national 5m 24s
+ update dev.ip_medicare_rx_cost_update c
+   set total_allowed_amount = a.tot_rx_cst_amt::numeric,
+   		total_charge_amount = null
+  from medicare_national.pde_file a
+  join data_warehouse.dim_uth_rx_claim_id b
+    on b.data_source in ('mcrn')
+   and a.bene_id = b.member_id_src 
+   and a.pde_id = b.rx_claim_id_src
+  where c.uth_rx_claim_id = b.uth_rx_claim_id;
+
+-- QA counting claims that the costs were not updated. if updated correctly count should be 0/empty table
+-- 5m
+select b.data_source, count(b.data_source)--a.uth_rx_claim_id, a.total_charge_amount, a.total_allowed_amount, b.total_charge_amount, b.total_allowed_amount
+from dev.ip_medicare_rx_cost_update a
+join data_warehouse.pharmacy_claims b
+on a.uth_rx_claim_id = b.uth_rx_claim_id
+and (a.total_allowed_amount = b.total_allowed_amount 
+ or a.total_charge_amount = b.total_charge_amount)
+group by 1;
