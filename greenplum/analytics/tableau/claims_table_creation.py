77e04a0ab5c4b1db4ab4d2b1d9c7a3ffd5db56e1
isrraelp <107650310+isrraelp@users.noreply.github.com>
2023-06-22 10:56:57 -0500

    up claim creation script
    adding keepalive param for connection to make sure connection is active while performing some queries

--- greenplum/analytics/tableau/claims_table_creation.py
+++ greenplum/analytics/tableau/claims_table_creation.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import psycopg2
+import select
 from tqdm import tqdm
 import sys
 sys.path.append('H:/uth_helpers/')
+import keyring
+from dotenv import dotenv_values
 from db_utils import get_dsn
 
 def create_temp_table(cursor, data_source):
@@ -56,7 +59,7 @@ def create_dx_pivot_table(cursor, data_source, year):
     drop_current_pivot_table = f'drop table if exists dev.ip_pivot_dx_temp_{data_source};'
 
     insert_query = f'''
-    insert into dev.ip_dx_temp_{data_source} 
+    insert into dev.ip_dx_temp_{data_source}
     (year, uth_claim_id, diag_cd, diag_position)	
     select extract(year from from_date_of_service), uth_claim_id, diag_cd, diag_position
     from data_warehouse.claim_diag
@@ -70,10 +73,13 @@ def create_dx_pivot_table(cursor, data_source, year):
 
     pivot_table_query = f'''select madlib.pivot('dev.ip_dx_temp_{data_source}', 'dev.ip_pivot_dx_temp_{data_source}', 'uth_claim_id', 'diag_position', 'diag_cd', 'max');'''
 
+    print('cleaning')
     cursor.execute(delete_current_rows)
     cursor.execute(drop_current_pivot_table)
+    print('inserting dx')
     cursor.execute(insert_query)
     cursor.execute(vacuum_query)
+    print('creating pivot table')
     cursor.execute(pivot_table_query)
 
 def dx_columns(connection):
@@ -130,7 +136,7 @@ def fill_claims_table(connection, data_source, year):
     '''
 
         cursor.execute(insert_query)
-        cursor.execute(f'vacuum analyze dev.ip_master_temp_{data_source};')
+        # cursor.execute(f'vacuum analyze dev.ip_master_temp_{data_source};')
 
 def swap_partitions(cursor, data_source):
     # once we gotten the refreshed data into a seperate table, swap current partition with our new table
@@ -151,27 +157,38 @@ def drop_temp_tables(cursor, data_source):
 
 if __name__ == '__main__':
 
-    connection = psycopg2.connect(get_dsn())
+    # connection = psycopg2.connect(get_dsn()+' tcp_keepalives_idle=60*30 keepalives_idle=60')
+    config = dotenv_values('H:/uth_helpers/dw.env')
+    connection = psycopg2.connect(
+                                    database=config['dbname'],
+                                    host=config['host'],
+                                    port=config['port'],
+                                    user=config['user'],
+                                    password=keyring.get_password(config['dbname'], config['user']),
+                                    keepalives=1,
+                                    keepalives_idle=30,
+                                    keepalives_interval=10
+                                    )
     connection.autocommit = True
     # 'optz', 'truv', 'mcrt', 'mcrn', 'mdcd', 'mhtw', 'mcpp'
-    data_sources = ['optd']
-
+    data_sources = ['truv']
+    # connection.close()
     #note that not all data sources have data for the same years. example: mcpp starts at 2015
-    years = tqdm(range(2015, 2022)) #{'start_year':2012, 'end_year':2021}
+     #{'start_year':2012, 'end_year':2021}
     
     for data_source in data_sources:
         print(data_source)
         with connection.cursor() as cursor:
-            create_temp_table(cursor, data_source)
+            # create_temp_table(cursor, data_source)
             create_dx_table(cursor, data_source)
-        
+        years = tqdm(range(2015, 2019))
         for year in years:
             years.set_description(str(year))
             with connection.cursor() as cursor:
                 create_dx_pivot_table(cursor, data_source, year)
-        
+            
             fill_claims_table(connection, data_source, year)
 
-        with connection.cursor() as cursor:
+        # with connection.cursor() as cursor:
             # swap_partitions(cursor, data_source)
-            drop_temp_tables(cursor, data_source)
+            # drop_temp_tables(cursor, data_source)
