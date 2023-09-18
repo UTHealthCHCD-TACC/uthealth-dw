ef51c49b00b38e155989af7ffc780ce23b754eef
isrraelp <107650310+isrraelp@users.noreply.github.com>
2023-09-18 16:11:46 -0500

    Update dw_ip_window_step_2.py

--- greenplum/datawarehouse/ip_window/dw_ip_window_step_2.py
+++ greenplum/datawarehouse/ip_window/dw_ip_window_step_2.py
@@ -207,7 +207,7 @@ def ip_window_wrapper(clm_df):
                                                       member_id_src=('member_id_src', 'first'))
     
     final_ip_group['missing_terminal_status'] = ~((final_ip_group['min_bill_type'].isin(['111', '114', '117'])) & (final_ip_group['max_bill_type'].isin(['111', '114', '117'])))
-    final_ip_group['missing_terminal_status_117'] = (final_ip_group['min_bill_type'] != final_ip_group['max_bill_type']) & (final_ip_group['max_bill_type'] == '117' | final_ip_group['min_bill_type'] == '117')
+    final_ip_group['missing_terminal_status_117'] = (final_ip_group['min_bill_type'] != final_ip_group['max_bill_type']) & (final_ip_group['max_bill_type'].isin(['117']) | final_ip_group['min_bill_type'].isin(['117']))
     print(final_ip_group.shape[0])
 
     # set in column order for load
