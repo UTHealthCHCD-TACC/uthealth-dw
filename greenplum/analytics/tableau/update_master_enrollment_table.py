0a335f52b4fd0d1c28321bf339e769d3a90a5148
isrraelp <107650310+isrraelp@users.noreply.github.com>
2023-09-28 14:21:15 -0500

    Update update_master_enrollment_table.py

--- greenplum/analytics/tableau/update_master_enrollment_table.py
+++ greenplum/analytics/tableau/update_master_enrollment_table.py
@@ -63,4 +63,162 @@ select e.*, c.aimm, c.ami, c.ca, c.cfib, c.chf, c.ckd, c.cliv, c.copd,
    on e.uth_member_id = cs.uth_member_id
   and e.year = cs.year
   and e.data_source = cs.data_source
-;'''
+;
+    '''
+
+    cursor.execute(query)
+
+    rowcount = cursor.rowcount
+
+    cursor.execute('vacuum analyze tableau.master_enrollment;')
+
+    return rowcount
+
+def qa(connection):
+    # Comparing With Member Enrollment Yearly Table
+    query = '''
+with dw as (
+    select data_source, year, count(distinct uth_member_id) dw_member_count, count(*) dw_row_count
+    from data_warehouse.member_enrollment_yearly
+    where year >= 2014
+    group by 1,2
+),
+tableau as (
+    select data_source, year, count(distinct uth_member_id) tableau_member_count, count(*) tableau_row_count
+    from tableau.master_enrollment
+    group by 1,2
+)
+select a.*, b.dw_member_count, b.dw_row_count
+from tableau a
+join dw b
+on a.data_source = b.data_source
+and a.year = b.year
+order by 1,2
+;
+    '''
+
+    df = pd.read_sql(query, con=connection)
+
+    df['member_check'] = df['dw_member_count'] == df['tableau_member_count']
+    df['row_check'] = df['dw_row_count'] == df['tableau_row_count']
+
+    print('Enrollment Table Mismatch')
+    print(df[~df['member_check'] | ~df['row_check']])
+
+
+    # Comparing With Conditions Member Enrollment Yearly Table
+    query = '''
+with dw as (
+    select data_source, year, count(distinct uth_member_id) dw_member_count, count(*) dw_row_count
+    from data_warehouse.conditions_member_enrollment_yearly
+    where year >= 2014
+    group by 1,2
+),
+tableau as (
+    select data_source, year, count(distinct uth_member_id) tableau_member_count, count(*) tableau_row_count
+    from tableau.master_enrollment
+    group by 1,2
+)
+select a.*, b.dw_member_count, b.dw_row_count
+from tableau a
+join dw b
+on a.data_source = b.data_source
+and a.year = b.year
+order by 1,2
+;
+    '''
+
+    df = pd.read_sql(query, con=connection)
+
+    df['member_check'] = df['dw_member_count'] == df['tableau_member_count']
+    df['row_check'] = df['dw_row_count'] == df['tableau_row_count']
+
+    print('Conditions Enrollment Table Mismatch')
+    print(df[~df['member_check'] | ~df['row_check']])
+
+    # Comparing With Covid Severity Table
+    query = '''
+with dw as (
+    select data_source, year, count(distinct uth_member_id) dw_member_count, count(*) dw_row_count
+    from data_warehouse.covid_severity
+    where year >= 2020
+    group by 1,2
+),
+tableau as (
+    select data_source, year, count(distinct uth_member_id) tableau_member_count, count(*) tableau_row_count
+    from tableau.master_enrollment
+    where year >= 2020
+    group by 1,2
+)
+select a.*, b.dw_member_count, b.dw_row_count
+from tableau a
+join dw b
+on a.data_source = b.data_source
+and a.year = b.year
+order by 1,2
+;
+    '''
+
+    df = pd.read_sql(query, con=connection)
+
+    df['member_check'] = df['dw_member_count'] == df['tableau_member_count']
+    df['row_check'] = df['dw_row_count'] == df['tableau_row_count']
+
+    print('COVID Severity Table Mismatch')
+    print(df[~df['member_check'] | ~df['row_check']])
+
+    # Comparing With CRG Table
+    query = '''
+with dw as (
+    select data_source, year, count(distinct uth_member_id) dw_member_count, count(*) dw_row_count
+    from data_warehouse.crg_risk
+    where year >= 2014
+    group by 1,2
+),
+tableau as (
+    select data_source, year, count(distinct uth_member_id) tableau_member_count, count(*) tableau_row_count
+    from tableau.master_enrollment
+    group by 1,2
+)
+select a.*, b.dw_member_count, b.dw_row_count
+from tableau a
+join dw b
+on a.data_source = b.data_source
+and a.year = b.year
+order by 1,2
+;
+    '''
+
+    df = pd.read_sql(query, con=connection)
+
+    df['member_check'] = df['dw_member_count'] == df['tableau_member_count']
+    df['row_check'] = df['dw_row_count'] == df['tableau_row_count']
+
+    print('CRG Table Mismatch')
+    print(df[~df['member_check'] | ~df['row_check']])
+
+
+if __name__ == '__main__':
+    connection = psycopg2.connect(get_dsn()+' keepalives=1 keepalives_idle=30 keepalives_interval=10')
+    connection.autocommit = True
+
+    data_sources = [
+        # 'optz',
+        # 'truc',
+        # 'trum',
+        # 'mcrt',
+        # 'mcrn', 
+        # 'mdcd',
+        # 'mhtw',
+        # 'mcpp'
+        ]
+
+    for data_source in data_sources:
+        print(data_source)
+        with connection.cursor() as cursor:
+            print(f'Number of {data_source} Rows deleted: ', delete_old_data(cursor, data_source))
+            print(f'Number of {data_source} Rows inserted: ', insert_new_data(cursor, data_source))
+
+    qa(connection)
+
+    connection.close()
