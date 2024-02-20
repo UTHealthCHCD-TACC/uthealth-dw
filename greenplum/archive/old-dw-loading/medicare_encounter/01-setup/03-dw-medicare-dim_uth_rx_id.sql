6d58f2bb6e9a95ed6bd3b88bc7e18640d0d516bf
isrraelp <107650310+isrraelp@users.noreply.github.com>
2024-02-20 15:43:17 -0600

    moving medicare_encounter scripts to archive

---
+++ greenplum/archive/old-dw-loading/medicare_encounter/01-setup/03-dw-medicare-dim_uth_rx_id.sql
@@ -0,0 +1,119 @@
+/**************************************
+ * This script assigns new uth_rx_claim_ids to claims that are not already in the table
+ * from Medicare Texas and Medicare National
+ * 
+ * 05/23/23: Xiaorui added timestamps and auto-updating the update_log (with backup!)
+ * And fixed a typo for adding mcrt claims - it was pulling data from the medicare national pde_file
+ * Original code is from Will though
+ * 
+ * ~ 5 minutes
+ * 
+ * --10/9/23 XZ: noted that script causes dupes to emerge, but haven't figured out why yet.
+ * Check if it happens again on next load. Might have something to do with year - we had issues
+ * with rx claims in 2019 whose member enrollment is in 2020 being duplicated
+ * --12/15/23 XZ: Previous issue did not reappear it looks like? Let's consider it fixed?
+ **************************************/
+
+select 'Medicare Texas dim_uth_rx_claim_id script started at ' || current_timestamp as message;
+
+with cte as (  
+    select distinct on (pde_id) 
+        year, pde_id, bene_id 
+    from medicare_texas.pde_file
+    )
+insert into data_warehouse.dim_uth_rx_claim_id (
+			 data_source
+			,year 
+			,uth_rx_claim_id
+			,rx_claim_id_src
+			,uth_member_id
+			,member_id_src ) 					
+select 'mcrt'
+       ,a.year::int
+       ,nextval('data_warehouse.dim_uth_rx_claim_id_uth_rx_claim_id_seq')
+	   ,a.pde_id
+	   ,b.uth_member_id
+	   ,a.bene_id 
+from cte a
+  join data_warehouse.dim_uth_member_id b 
+    on b.data_source = 'mcrt'
+   and b.member_id_src = a.bene_id
+left join data_warehouse.dim_uth_rx_claim_id c 
+  on c.data_source = 'mcrt'
+ and c.member_id_src = a.bene_id 
+ and c.rx_claim_id_src = a.pde_id
+where c.uth_rx_claim_id is null 
+  and a.bene_id is not null
+ ;
+
+select 'Medicare Texas finished, Medicare National started at ' || current_timestamp as message;
+
+---Medicare National
+with cte as (  
+    select distinct on (pde_id) 
+        year, pde_id, bene_id 
+    from medicare_national.pde_file
+    )
+insert into data_warehouse.dim_uth_rx_claim_id (
+			 data_source
+			,year 
+			,uth_rx_claim_id
+			,rx_claim_id_src
+			,uth_member_id
+			,member_id_src ) 					
+select 'mcrn'
+       ,a.year::int
+       ,nextval('data_warehouse.dim_uth_rx_claim_id_uth_rx_claim_id_seq')
+	   ,a.pde_id
+	   ,b.uth_member_id
+	   ,a.bene_id 
+from cte a
+  join data_warehouse.dim_uth_member_id b 
+    on b.data_source = 'mcrn'
+   and b.member_id_src = a.bene_id
+left join data_warehouse.dim_uth_rx_claim_id c 
+  on c.data_source = 'mcrn'
+ and c.member_id_src = a.bene_id 
+ and c.rx_claim_id_src = a.pde_id
+where c.uth_rx_claim_id is null 
+  and a.bene_id is not null
+ ;
+
+select 'Medicare National dim_uth_rx_claim_id script finished at ' || current_timestamp as message;
+
+vacuum analyze data_warehouse.dim_uth_rx_claim_id;
+
+--check last vacuum analyze to see if it worked
+/*
+select schemaname, relname, last_vacuum, last_analyze
+from pg_stat_all_tables
+where schemaname = 'data_warehouse' and relname = 'dim_uth_rx_claim_id';
+*/
+
+--backup update_log
+drop table if exists backup.update_log;
+
+create table backup.update_log as
+select * from data_warehouse.update_log;
+
+--update update_log
+update data_warehouse.update_log a
+set data_last_updated = current_date,
+	details = 'Medicare Texas and Medicare National updated',
+	last_vacuum_analyze = case when b.last_vacuum is not null then b.last_vacuum else b.last_analyze end
+from pg_catalog.pg_stat_all_tables b
+where a.schema_name = b.schemaname and a.table_name = b.relname
+and schema_name = 'data_warehouse' and table_name = 'dim_uth_rx_claim_id';
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ greenplum/archive/old-dw-loading/medicare_encounter/02-enrollment/01-dw-mcrt-member_enrollment_monthly.sql
@@ -0,0 +1,264 @@
+/* ******************************************************************************************************
+ *  The member_enrollment_monthly table creates one record for each month/year that a member was enrolled in coverage
+ * 
+ *  !!!!!!!!!  data_warehouse.dim_member_id_src table must be populated first !!!!!!!!!    
+ *   	             See folder 01-setup
+ * 	
+ * ******************************************************************************************************
+ *  Author || Date      || Notes
+ * ******************************************************************************************************
+ *  wc001  || 1/01/2021 || script created 
+ * ******************************************************************************************************
+ *  wc002  || 6/28/21 || added logic to exclude enrollment records after death optum dod
+ * ******************************************************************************************************
+ *  wallingTACC  || 8/23/2021 || Cleaning up comments
+ * ******************************************************************************************************
+ *  wc003  || 9/02/2021 || Changing process to load dw_staging. Add mapping for null race to assign 0 (Unknown).
+ * ******************************************************************************************************
+ *  jw001  || 9/20/2021 || Cut to its own script file from longer file
+ *  ******************************************************************************************************
+ *  wc004  || 11/06/2021 || moved table creation to new script. formatting. changed bus_cd mapping
+ *  ******************************************************************************************************
+ *  xzhang || 05/25/23   || Tweaked for 2023
+ * 							split medicare texas and medicare national into their own sql files
+ * 							split into individual tables for easier insertion into DW
+ * 							age_derived -> age_cy, and changed calculation method
+ * 							added age_months, age_fy, table_id_src, member_id_src, load_date
+ * 							added consecutive enrolled months
+ * 							added a vacuum analyze
+ *  ******************************************************************************************************
+ *  xzhang || 05/31/23   || There was an issue with a join and I didn't want to try to troubleshoot the legacy code
+ * 							so I re-wrote it so that an ETL is generated first (make enrollment table long
+ * 							rather than wide) and THEN the final table is generated
+ * *******************************************************************************************************
+ *  xzhang || 06/23/23	 || After some discussion with Lopita/TMK, we're going to make the following changes:
+ * 							Medicare part C - add in PTC_CNTRCT_ID ('N' = no part C, otherwise there is a contract id which means yes part C)
+ * 							Change part D contract b/c it works the same way as part C, there's no need
+ * 							to join on the first letter. Null/N/0 = not enrolled, otherwise yes enrolled
+ * 							Fill in Dual column - this necessitates a data_type change for the base table from int -> bpchar(1)
+ * 							And duals will be divided into non-dual, full dual, and partial dual as per
+ * 							RESDAC's documentation
+ * 						
+*/
+
+/* Change dual column from int to bpchar(1)
+alter table data_warehouse.member_enrollment_yearly alter column dual type bpchar(1);
+alter table data_warehouse.member_enrollment_fiscal_yearly alter column dual type bpchar(1);
+alter table data_warehouse.member_enrollment_monthly alter column dual type bpchar(1);
+
+vacuum analyze data_warehouse.member_enrollment_monthly;
+vacuum analyze data_warehouse.member_enrollment_yearly;
+vacuum analyze data_warehouse.member_enrollment_fiscal_yearly;
+ */
+
+/******************************
+ * MEDICARE TEXAS
+ ******************************/
+
+--Medicare enrollment tables are wide rather than long, so let's fix that
+--First, rearrange enrollment table into a long table
+drop table if exists dw_staging.mcrt_member_enrollment_monthly_etl;
+
+create table dw_staging.mcrt_member_enrollment_monthly_etl (
+	year int2,
+	bene_id text,
+	dob date,
+	dod date,
+	sex text,
+	state text,
+	zip text,
+	race text,
+	month_year_id varchar(8),
+	mdcr_status_code varchar(8), --determines if member is enrolled in Medicare
+	mdcr_entlmt_buyin_ind varchar(8), --determines coverage - A, B, or A&B
+	ptc_cntrct_id varchar(8), --determines if member has Medicare part C
+	ptd_cntrct_id varchar(8), --determines if member has Medicare part D
+	dual_stus_cd varchar(8) --determines if member is dually-enrolled with Medicaid
+)
+distributed by (bene_id);
+
+do $$
+declare
+	i int := 1;
+	month char(2);
+begin
+	raise notice 'Elongating Medicare Texas enrollment table...';
+	
+	for i in 1..12
+	loop
+	    month := lpad(i::text, 2, '0');
+		execute 'insert into dw_staging.mcrt_member_enrollment_monthly_etl
+			select bene_enrollmt_ref_yr::int, bene_id,
+				bene_birth_dt::date, bene_death_dt::date,
+				sex_ident_cd, state_code, zip_cd, bene_race_cd,
+				year || ''' || month || ''' as month_year_id,
+				mdcr_status_code_' || month || ',
+				mdcr_entlmt_buyin_ind_' || month || ',
+				ptc_cntrct_id_' || month || ',
+				ptd_cntrct_id_' || month || ',
+				dual_stus_cd_' || month || '
+			from medicare_texas.mbsf_abcd_summary
+			where mdcr_status_code_' || month || ' in (''10'',''11'',''20'',''21'',''31'');'
+					;
+		raise notice 'Month % completed', month;
+	end loop;
+
+	raise notice 'Medicare Texas enrollment table elongation completed!';
+end $$;
+
+vacuum analyze dw_staging.mcrt_member_enrollment_monthly_etl;
+
+--select * from dw_staging.mcrt_member_enrollment_monthly_etl order by year, bene_id limit 10;
+
+/************
+ * Make monthly enrollment table
+ *   - Join in dim_uth_member_id
+ *   - Join in ref_month_year (to calculate ages, etc)
+ *   - Join in race code
+ *   - Join in state code
+ */
+
+drop table if exists dw_staging.mcrt_member_enrollment_monthly;
+
+create table dw_staging.mcrt_member_enrollment_monthly 
+(like data_warehouse.member_enrollment_monthly including defaults) 
+with (
+		appendonly=true, 
+		orientation=row, 
+		compresstype=zlib, 
+		compresslevel=5 
+	 )
+distributed by (uth_member_id)
+;
+
+insert into dw_staging.mcrt_member_enrollment_monthly (
+	data_source, 
+	year, 
+	month_year_id, 
+	uth_member_id,
+	gender_cd, 
+	state, 
+	zip5, 
+	zip3,
+	age_cy, 
+	dob_derived, 
+	death_date,
+	plan_type, 
+	bus_cd, 
+	rx_coverage, 
+	fiscal_year, 
+	race_cd,
+	load_date,
+	age_months,
+	dual,
+	table_id_src,
+	member_id_src,
+	age_fy
+	)		
+select 'mcrt' as data_source,
+	   a.year, 
+	   c.month_year_id, 
+	   b.uth_member_id,
+	   case when a.sex = '0' then 'U'
+	   		when a.sex = '1' then 'M'
+	   		when a.sex = '2' then 'F' end as gender_cd,
+	   e.state_cd as state,
+	   a.zip as zip5, 
+	   substring(a.zip,1,3) as zip3,
+  	   extract(years from age(c.cy_end, a.dob)) as age_cy,
+	   a.dob as dob_derived, 
+	   a.dod as death_date,
+	   --plan type
+	   case when ptc_cntrct_id = 'N' then g.plan_type
+	   		else 'C' end as plan_type, 
+	   null as bus_cd,
+	   --part D coverage
+	   case when ptd_cntrct_id is null or ptd_cntrct_id in ('0', 'N') then 0 else 1
+			end as rx_coverage,
+	   c.fy_ut as fiscal_year, 
+	   f.race_cd,
+	   current_date as load_date,
+	   extract(years from age(to_date(a.month_year_id::text, 'YYYYMM'), a.dob)) * 12 + 
+	   extract(months from age(to_date(a.month_year_id::text, 'YYYYMM'), a.dob)) as age_months,
+	   case when a.dual_stus_cd in ('02', '04', '08') then '1'
+	   		when a.dual_stus_cd in ('01', '03', '05', '06') then 'P'
+	   		else '0' end as dual,
+	   'medicare_texas.mbsf_abcd_summary' as table_id_src,
+	   a.bene_id as member_id_src,
+  	   extract(years from age(c.fy_end, a.dob)) as age_fy
+from dw_staging.mcrt_member_enrollment_monthly_etl a
+  left join data_warehouse.dim_uth_member_id b
+    on a.bene_id::text = b.member_id_src
+   and b.data_source = 'mcrt'
+  left join reference_tables.ref_month_year c
+  	on a.month_year_id::int = c.month_year_id
+  left join reference_tables.ref_medicare_state_codes e
+     on a.state = e.medicare_state_cd
+  left join reference_tables.ref_race f 
+     on f.race_cd_src = a.race 
+    and f.data_source = 'mcrt'
+  left join reference_tables.ref_medicare_entlmt_buyin g 
+    on g.buyin_cd = a.mdcr_entlmt_buyin_ind
+;
+
+analyze dw_staging.mcrt_member_enrollment_monthly;
+
+/****************
+ * CHECKPOINT: did the table joins generate a lot of nulls?
+ */
+
+select count(*), sum(case when uth_member_id is null then 1 else 0 end) as uth_memid,
+	sum(case when month_year_id is null then 1 else 0 end) as monyr_id,
+	sum(case when gender_cd is null then 1 else 0 end) as sex,
+	sum(case when state is null then 1 else 0 end) as state,
+	sum(case when race_cd is null then 1 else 0 end) as race,
+	sum(case when plan_type is null then 1 else 0 end) as plan,
+	sum(case when rx_coverage is null then 1 else 0 end) as rx_cvg,
+	sum(case when uth_member_id is null then 1 else 0 end)*1.0/count(*) as uth_memid_pct,
+	sum(case when month_year_id is null then 1 else 0 end)*1.0/count(*) as monyr_id_pct,
+	sum(case when gender_cd is null then 1 else 0 end)*1.0/count(*) as sex_pct,
+	sum(case when state is null then 1 else 0 end)*1.0/count(*) as state_pct,
+	sum(case when race_cd is null then 1 else 0 end)*1.0/count(*) as race_pct,
+	sum(case when plan_type is null then 1 else 0 end)*1.0/count(*) as plan_pct,
+	sum(case when rx_coverage is null then 1 else 0 end)*1.0/count(*) as rx_cvg_pct
+from dw_staging.mcrt_member_enrollment_monthly;
+
+---**script to build consecutive enrolled months	
+drop table if exists dw_staging.temp_mcrt_consec_enrollment;
+
+create table dw_staging.temp_mcrt_consec_enrollment 
+with (appendonly=true, orientation=column) as 
+select month_year_id, uth_member_id
+      ,row_number() over(partition by uth_member_id, my_grp order by  month_year_id) as in_streak
+from ( 
+	   select a.month_year_id,
+	        a.uth_member_id,
+	        b.my_row_counter - row_number() over(partition by a.uth_member_id
+	        	order by a.month_year_id) as my_grp
+	   from dw_staging.mcrt_member_enrollment_monthly a 
+	     join reference_tables.ref_month_year b 
+	       on a.month_year_id = b.month_year_id 	   		    
+	 ) t    
+distributed by (month_year_id);
+
+analyze dw_staging.temp_mcrt_consec_enrollment;
+
+--update consec enrolled months
+update dw_staging.mcrt_member_enrollment_monthly a 
+   set consecutive_enrolled_months = b.in_streak 
+  from dw_staging.temp_mcrt_consec_enrollment b 
+ where a.month_year_id = b.month_year_id and a.uth_member_id = b.uth_member_id;
+
+--**cleanup
+drop table if exists dw_staging.temp_mcrt_consec_enrollment;
+
+--vacuum analyze
+vacuum analyze dw_staging.mcrt_member_enrollment_monthly;
+
+
+
+
+
+
+
+
